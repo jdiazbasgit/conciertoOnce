7702d4b4004e1a639ec6f416a2f26423066fa0bf
javier <javer@javier.com>
2021-12-15 12:51:29 +0100

    caraga inicial

---
+++ src/concierto/teatros/ConciertoSolista.java
@@ -0,0 +1,30 @@
+package concierto.teatros;
+
+import java.util.Iterator;
+
+import concierto.beans.InstrumentoBean;
+import concierto.instrumentos.Instrumento;
+import concierto.instrumentos.InstrumentoInterface;
+import concierto.musicos.Musico;
+
+public class ConciertoSolista {
+
+	public static void main(String[] args) {
+		
+
+		
+		InstrumentoBean instrumentoBean= new InstrumentoBean();
+		instrumentoBean.setSonido("pom, pom ,pom en bean");
+		instrumentoBean.setMarca("nueva marca");
+		instrumentoBean.setPrecio(500);
+		InstrumentoInterface tambor = new Instrumento(instrumentoBean);
+		//tambor.setInstrumentoBean(instrumentoBean);
+		
+		Musico solista= new Musico();
+		solista.setInstrumentoInterface(tambor);
+		
+		solista.tocar();
+
+	}
+
+}
