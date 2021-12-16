c9177e2c40514758dec92d4d108bdb50b45136c0
javier <javer@javier.com>
2021-12-16 14:13:49 +0100

    carga con gitignore

---
+++ src/concierto/instrumentos/InstrumentoInterface.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package concierto.instrumentos;
+
+import concierto.beans.InstrumentoBean;
+
+/**
+ * @author profe
+ *Interface que genera clases de instrumentos
+ */
+public interface InstrumentoInterface {
+
+	public String sonar();
+}
