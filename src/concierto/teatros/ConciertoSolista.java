decca919be6f91889cfd888092f4bf9903d944a7
javier <javer@javier.com>
2021-12-20 14:01:37 +0100

    inicio de proyecto

--- src/concierto/teatros/ConciertoSolista.java
+++ src/concierto/teatros/ConciertoSolista.java
@@ -16,34 +16,15 @@ public class ConciertoSolista {
 		instrumentoBeanTambor.setMarca("nueva marca");
 		instrumentoBeanTambor.setPrecio(500);
 		InstrumentoInterface tambor = new Instrumento(instrumentoBeanTambor);
-		// tambor.setInstrumentoBean(instrumentoBean);
+		
 
-		Musico solista = new Musico();
+		Musico solista = new Musico(tambor);
 		solista.setInstrumentoInterface(tambor);
 
-		InstrumentoBean instrumentoBeanTrompeta = new InstrumentoBean();
-		instrumentoBeanTrompeta.setSonido("tuuuu, tuuuu, tuuuu en bean");
-		instrumentoBeanTrompeta.setMarca("nueva marca");
-		instrumentoBeanTrompeta.setPrecio(500);
-		InstrumentoInterface trompeta = new Instrumento(instrumentoBeanTrompeta);
-		// tambor.setInstrumentoBean(instrumentoBean);
-
-		Musico solista1 = new Musico();
-		solista1.setInstrumentoInterface(trompeta);
-
-		InstrumentoBean instrumentoBeanGuitarra = new InstrumentoBean();
-		instrumentoBeanGuitarra.setSonido("tlan, tlan, tlan en bean");
-		instrumentoBeanGuitarra.setMarca("nueva marca");
-		instrumentoBeanGuitarra.setPrecio(500);
-		InstrumentoInterface guitarra = new Instrumento(instrumentoBeanGuitarra);
-		// tambor.setInstrumentoBean(instrumentoBean);
-
-		Musico solista2 = new Musico();
-		solista2.setInstrumentoInterface(guitarra);
-		
 		solista.tocar();
-		solista1.tocar();
-		solista2.tocar();
+		
+
+		
 
 	}
 
