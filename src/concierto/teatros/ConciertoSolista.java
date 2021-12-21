66b654b06a5ba2d8445b3016d08938dc99130607
Your Name <you@example.com>
2021-12-21 12:30:55 +0100

    ordenado

--- src/concierto/teatros/ConciertoSolista.java
+++ src/concierto/teatros/ConciertoSolista.java
@@ -15,12 +15,10 @@ public class ConciertoSolista {
 		InstrumentoBean instrumentoBeanTambor = new InstrumentoBean();
 		instrumentoBeanTambor.setSonido("pom, pom ,pom en bean");
 		InstrumentoInterface tambor = new Instrumento(instrumentoBeanTambor);
-
-		InstrumentoInterface hang = new InstrumentoPedro("wuon, wan, wuon");
-
 		Musico solista = new Musico(tambor);
 		solista.setInstrumentoInterface(tambor);
 
+		InstrumentoInterface hang = new InstrumentoPedro("wuon, wan, wuon");
 		MusicoInterface solistaPedro = new MusicoPedro(hang);
 
 		solista.tocar();
