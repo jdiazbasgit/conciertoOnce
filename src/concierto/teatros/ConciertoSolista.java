c7f08ea61343bbfde1f920cd2116d721d03ebcca
Your Name <you@example.com>
2021-12-21 12:46:16 +0100

    proyecto de Almu2

--- src/concierto/teatros/ConciertoSolista.java
+++ src/concierto/teatros/ConciertoSolista.java
@@ -1,11 +1,12 @@
 package concierto.teatros;
 
-import java.util.Iterator;
-
 import concierto.beans.InstrumentoBean;
 import concierto.instrumentos.Instrumento;
+import concierto.instrumentos.InstrumentoAlmu2;
 import concierto.instrumentos.InstrumentoInterface;
 import concierto.musicos.Musico;
+import concierto.musicos.MusicoAlmu2;
+import concierto.musicos.MusicoInterface;
 
 public class ConciertoSolista {
 
@@ -20,9 +21,14 @@ public class ConciertoSolista {
 
 		Musico solista = new Musico(tambor);
 		solista.setInstrumentoInterface(tambor);
+		
 
 		solista.tocar();
 		
+		
+		InstrumentoInterface instrumentoAlmu2 = new InstrumentoAlmu2("sonido instrumento almu");
+		MusicoInterface musicoAlmu2 = new MusicoAlmu2 (instrumentoAlmu2);
+		musicoAlmu2.tocar();
 
 		
 
