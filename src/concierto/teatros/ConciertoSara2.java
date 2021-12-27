a984129d204ce9169831791bbaaceb6f168f6d19
sara_ <sara_@DESKTOP-SOSLR4B>
2021-12-27 21:03:07 +0100

    h

--- src/concierto/teatros/ConciertoSara2.java
+++ src/concierto/teatros/ConciertoSara2.java
@@ -14,29 +14,29 @@ public class ConciertoSolista {
 
 	public static void main(String[] args, InstrumentoInterface flauta) {
 
-		InstrumentoSara1 instrumentoBeanTambor = new InstrumentoSara1();
-		instrumentoBeanTambor.setSonido("pom, pom ,pom en bean");
+		InstrumentoSara instrumentoBeanTambor = new InstrumentoSara();
+		instrumentoBeanTambor.setSonido("pom, pom ,pom");
 	
 		InstrumentoInterface tambor = new Instrumento(instrumentoBeanTambor);
-		// tambor.setInstrumentoBean(instrumentoBean);
+		
 
 		Musico solista = new Musico();
 		solista.setInstrumentoInterface(tambor);
 
-		InstrumentoSara1 instrumentoBeanTrompeta = new InstrumentoSara1();
-		instrumentoBeanTrompeta.setSonido("tuuuu, tuuuu, tuuuu en bean");
+		InstrumentoSara instrumentoBeanTrompeta = new InstrumentoSara();
+		instrumentoBeanTrompeta.setSonido("tuuuu, tuuuu, tuuuu");
 		
 		InstrumentoInterface trompeta = new Instrumento(instrumentoBeanTrompeta);
-		// tambor.setInstrumentoBean(instrumentoBean);
+	
 
 		Musico solista1 = new Musico();
 		solista1.setInstrumentoInterface(trompeta);
 
-		InstrumentoSara1 instrumentoBeanGuitarra = new InstrumentoSara1();
-		instrumentoBeanGuitarra.setSonido("tlan, tlan, tlan en bean");
+		InstrumentoSara instrumentoBeanGuitarra = new InstrumentoSara();
+		instrumentoBeanGuitarra.setSonido("tlan, tlan, tlan");
 		
 		InstrumentoInterface guitarra = new Instrumento(instrumentoBeanGuitarra);
-		// tambor.setInstrumentoBean(instrumentoBean);
+	
 
 		Musico solista2 = new Musico();
 		solista2.setInstrumentoInterface(guitarra);
@@ -45,8 +45,8 @@ public class ConciertoSolista {
 		Musico solista3 = new Musico();
 		solista3.setInstrumentoInterface(flauta);
 
-		InstrumentoSara1 instrumentoBeanflauta = new InstrumentoSara1();
-		instrumentoBeanflauta.setSonido("flu, flu, flu en bean");
+		InstrumentoSara instrumentoBeanflauta = new InstrumentoSara();
+		instrumentoBeanflauta.setSonido("flu, flu, flu");
 		
 	
 		
