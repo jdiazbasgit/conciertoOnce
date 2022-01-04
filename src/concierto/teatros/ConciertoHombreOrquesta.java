2db971779dcab2c39f42479c2f8ac81bf7212dfe
Javier <Javier@DESKTOP-30LQLRT>
2022-01-05 00:25:39 +0100

    cambio

--- src/concierto/teatros/ConciertoHombreOrquesta.java
+++ src/concierto/teatros/ConciertoHombreOrquesta.java
@@ -10,6 +10,15 @@ public class ConciertoHombreOrquesta {
 	public static void main(String[] args) {
 		InstrumentoInterface[] instrumentos = {new Instrumento(new InstrumentoBean("pom,pom,pom")), 
 				new Instrumento(new InstrumentoBean("tuuu, tuuu, tuuu")),
+				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
+				new Instrumento(new InstrumentoBean("pim, pam")),
+				new Instrumento(new InstrumentoBean("pluf, pluf")),
+				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
+				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
+				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
+				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
+				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
+				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
 				new Instrumento(new InstrumentoBean("tlan, tlan, tlan"))};
 		
 		HombreOrquesta hombreOrquesta = new HombreOrquesta(instrumentos);
