e407b4f2f3e025e43891033034cc8ba5f16dc226
ark4noid <arkano07@gmail.com>
2022-01-05 20:24:05 +0100

    ejercicio Navidad

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -5,29 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 
-//COMENTARIO PRUEBA
-
 public class Banco {
 
 	public static void main(String[] args) {
+		Operaciones operaciones = new Operaciones();
 
 		while (true) {
 			menu();
 			try {
 				int opcion = Integer.parseInt(leerTeclado());
 				switch (opcion) {
+				
 				case 1:
-
+					System.out.println("Cuanto quieres ingresar?");
+					double ingreso = Double.parseDouble(leerTeclado());
+					operaciones.ingresar(ingreso);
 					break;
+					
 				case 2:
-
+					System.out.println("Cuanto quieres retirar?");
+					double retiro = Double.parseDouble(leerTeclado());
+					operaciones.retirar(retiro);
 					break;
 
 				case 3:
-
+					System.out.println("El saldo es: " + operaciones.getSaldo());
 					break;
+					
 				case 4:
-
 					System.exit(0);
 
 				default:
@@ -49,12 +54,12 @@ public class Banco {
 	}
 
 	public static void menu() {
-
+		System.out.println("==================");
 		System.out.println("Menu del banco");
 		System.out.println("==================");
 		System.out.println("1.- Ingresar");
-		System.out.println("2.- Cobrar");
-		System.out.println("3.-saldo");
+		System.out.println("2.- Retirar");
+		System.out.println("3.- Consultar el saldo");
 		System.out.println("4.- Terminar");
 		System.out.println("Teclea opción:");
 	}
--- src/banco/Operaciones.java
+++ src/banco/Operaciones.java
@@ -1,5 +1,24 @@
 package banco;
 
 public class Operaciones {
+	private double saldo;
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void ingresar(double cantidadIngresada) {
+		saldo = saldo + cantidadIngresada;
+
+	}
+	 public void retirar(double cantidadRetirada) {
+		 saldo = saldo - cantidadRetirada;
+	 }
+	
+	
 
 }
