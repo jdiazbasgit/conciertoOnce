792d0517d0fc12a54771865b7c6d71329449ff01
toni <juanvegaros@outlook.com>
2022-01-07 01:12:03 +0100

    Revisión del ejercício del hombre orquesta

--- src/concierto/musicos/HombreOrquesta.java
+++ src/concierto/musicos/HombreOrquesta.java
@@ -1,5 +1,5 @@
 package concierto.musicos;
-
+import java.util.List;
 import concierto.instrumentos.InstrumentoInterface;
 
 public class HombreOrquesta extends Musico {
@@ -11,19 +11,18 @@ public class HombreOrquesta extends Musico {
 	@Override
 	public void tocar() {
 
-		for (int i = 0; i < getInstrumentosInterface().length; i++) {
-				System.out.println(getInstrumentosInterface()[i].sonar());
-		}
+//		for (int i = 0; i < getInstrumentosInterface().length; i++) {
+//				System.out.println(getInstrumentosInterface()[i].sonar());
+//		}
 
-		/*
-		 * for (InstrumentoInterface instrumento : getInstrumentosInterface()) {
-		 * System.out.println(instrumento.sonar()); }
-		 */
-
-		/*
-		 * List.of(getInstrumentosInterface()).stream().filter(i->i.sonar().length()>12)
-		 * .forEach(instrumento->{ System.out.println(instrumento.sonar()); });
-		 */
+		
+//		 for (InstrumentoInterface instrumento : getInstrumentosInterface()) {
+//		 System.out.println(instrumento.sonar()); 
+//		 }
+		 
+		  List.of(getInstrumentosInterface()).stream()
+		  .forEach(instrumento->{ System.out.println(instrumento.sonar()); });
+		 
 	}
 
 }
