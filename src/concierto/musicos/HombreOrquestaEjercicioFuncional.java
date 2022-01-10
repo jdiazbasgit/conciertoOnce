de86ddc2799f2417dc0c46fc2b688bc722329661
sara_ <sara_@DESKTOP-SOSLR4B>
2022-01-10 09:55:09 +0100

    Solucion al ejercicio.

--- src/concierto/musicos/HombreOrquestaEjercicioFuncional.java
+++ src/concierto/musicos/HombreOrquestaEjercicioFuncional.java
@@ -3,18 +3,26 @@ package concierto.musicos;
 import java.util.List;
 
 import concierto.instrumentos.Instrumento;
+import concierto.instrumentos.InstrumentoInterface;
 
 public class HombreOrquestaEjercicioFuncional extends Musico {
 
-	public HombreOrquestaEjercicioFuncional(Instrumento instrumento) {
-		super(instrumento);
-		
+	
+
+	public HombreOrquestaEjercicioFuncional(InstrumentoInterface instrumentoInterface) {
+		super(instrumentoInterface);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HombreOrquestaEjercicioFuncional(InstrumentoInterface[] instrumentosInterface) {
+		super(instrumentosInterface);
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void tocar() {
 		
-		 List.of(getInstrumentosInterface()).stream().filter(i->i.sonar().length()>12)
+		 List.of(getInstrumentosInterface()).stream().filter(i->i.sonar().length()>5)
 		 .forEach(instrumento->{ System.out.println(instrumento.sonar()); });
 		 
 	}}
--- src/concierto/teatro/ConciertoHombreOrquestaEjercicioFuncional.java
+++ src/concierto/teatro/ConciertoHombreOrquestaEjercicioFuncional.java
@@ -1,4 +1,5 @@
 package concierto.teatro;
+
 import concierto.beans.InstrumentoBean;
 import concierto.instrumentos.Instrumento;
 import concierto.instrumentos.InstrumentoInterface;
@@ -6,19 +7,19 @@ import concierto.musicos.HombreOrquestaEjercicioFuncional;
 
 public class ConciertoHombreOrquestaEjercicioFuncional {
 
-		public static void main(String[] args) {
-		 InstrumentoInterface[] instrumentos= {
-				 new Instrumento(new InstrumentoBean("tim,tim,tim")),
-				 new Instrumento(new InstrumentoBean("tan,tan,tan")),
-				 new Instrumento(new InstrumentoBean("chin,chin,chin")), 
-		         new Instrumento(new InstrumentoBean("flu,flu,flu")),
-		         new Instrumento(new InstrumentoBean("cha,cha,cha")),
-		         new Instrumento(new InstrumentoBean("tsh,tsh,tsh")),
-		         new Instrumento(new InstrumentoBean("cloc,cloc,cloc")),
-				 new Instrumento(new InstrumentoBean("tilin,tilin,tilin")),
-				 new Instrumento(new InstrumentoBean("la,la,la")),
-				 new Instrumento(new InstrumentoBean("da,de,di,do,du"))};
-		
-		 new HombreOrquestaEjercicioFuncional(new Instrumento("tim,tim,tim")).tocar();}
+	public static void main(String[] args) {
+		InstrumentoInterface[] instrumentos = { new Instrumento(new InstrumentoBean("tim,tim,tim")),
+				new Instrumento(new InstrumentoBean("tan,tan,tan")),
+				new Instrumento(new InstrumentoBean("chin,chin,chin")),
+				new Instrumento(new InstrumentoBean("flu,flu,flu")),
+				new Instrumento(new InstrumentoBean("cha,cha,cha")),
+				new Instrumento(new InstrumentoBean("tsh,tsh,tsh")),
+				new Instrumento(new InstrumentoBean("cloc,cloc,cloc")),
+				new Instrumento(new InstrumentoBean("tilin,tilin,tilin")),
+				new Instrumento(new InstrumentoBean("la,la,la,la")),
+				new Instrumento(new InstrumentoBean("da,de,di,do,du")) };
 
+		new HombreOrquestaEjercicioFuncional(instrumentos).tocar();
 	}
+
+}
