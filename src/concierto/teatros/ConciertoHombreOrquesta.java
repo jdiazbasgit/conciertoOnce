bcf25afddf2faddf24cedb2a1fa407860015247d
Javier <Javier@DESKTOP-30LQLRT>
2022-01-10 09:31:50 +0100

    cambios

--- src/concierto/teatros/ConciertoHombreOrquesta.java
+++ src/concierto/teatros/ConciertoHombreOrquesta.java
@@ -10,16 +10,15 @@ public class ConciertoHombreOrquesta {
 	public static void main(String[] args) {
 		InstrumentoInterface[] instrumentos = {new Instrumento(new InstrumentoBean("pom,pom,pom")), 
 				new Instrumento(new InstrumentoBean("tuuu, tuuu, tuuu")),
-				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
-				new Instrumento(new InstrumentoBean("pim, pam")),
+				new Instrumento(new InstrumentoBean("pi, pi, pi")),
+				new Instrumento(new InstrumentoBean("pam, pam")),
 				new Instrumento(new InstrumentoBean("pluf, pluf")),
+				new Instrumento(new InstrumentoBean("plin, plin")),
 				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
-				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
-				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
-				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
-				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
-				new Instrumento(new InstrumentoBean("tlan, tlan, tlan")),
-				new Instrumento(new InstrumentoBean("tlan, tlan, tlan"))};
+				new Instrumento(new InstrumentoBean("pla, pla, pla")),
+				new Instrumento(new InstrumentoBean("da, da, da")),
+				new Instrumento(new InstrumentoBean("flun, flun")),
+				new Instrumento(new InstrumentoBean("pa , pa"))};
 		
 		HombreOrquesta hombreOrquesta = new HombreOrquesta(instrumentos);
 		
