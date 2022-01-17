d0b7d959c2a25eefa114f07237839434627609b8
sara_ <sara_@DESKTOP-SOSLR4B>
2022-01-17 17:37:31 +0100

    falta la excepcion

---
+++ src/banco/Bancouno.java
@@ -0,0 +1,63 @@
+package banco;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bancouno {
+
+	public static void main(String[] args) {
+
+		CuentaBancaria cuenta = new CuentaBancaria();
+
+		while (true) {
+			menu();
+			try {
+				int opcion = Integer.parseInt(leerTeclado());
+				switch (opcion) {
+				case 1:
+					System.out.println("Introduce cantidad a ingresar : ");
+					double ingreso = Double.parseDouble(leerTeclado());
+					cuenta.ingresar(ingreso);
+					break;
+				case 2:
+					System.out.println("Introduce cantidad a sacar : ");
+					double cantidad = Double.parseDouble(leerTeclado());
+					cuenta.sacarDinero(cantidad);
+					break;
+				case 3:
+					System.out.println("Saldo Cuenta : " + cuenta.getSaldo());
+					break;
+				case 4:
+					System.exit(0);
+
+				default:
+					System.out.println("opcion no valida");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("debes escribir un numero");
+			}
+		}
+	}
+
+	private static String leerTeclado() {
+
+		try {
+			return new BufferedReader(new InputStreamReader(System.in)).readLine();
+		} catch (IOException e) {
+			return null;
+		}
+	}
+
+	public static void menu() {
+
+		System.out.println("Menu del banco");
+		System.out.println("==================");
+		System.out.println("1.- Ingresar");
+		System.out.println("2.- Cobrar");
+		System.out.println("3.-saldo");
+		System.out.println("4.- Terminar");
+		System.out.println("Teclea opcion:");
+	}
+
+}
--- src/banco/CuentaBancaria.java
+++ src/banco/CuentaBancaria.java
@@ -1,10 +1,42 @@
 package banco;
 
 public class CuentaBancaria {
-public static void main(String[]args) {
-	int total;
-	
-	Cuenta CuentaBancaria = new Cuenta(22222,(int) 4900.56);
-}
+	private double saldo;
 
+	public CuentaBancaria() {
+		setSaldo(0.0);
+	}
+
+	public CuentaBancaria(double saldo) {
+		super();
+		setSaldo(saldo);
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		if (saldo >= 0) {
+			this.saldo = saldo;
+			return;
+		}
+		System.out.println("El saldo no puede ser negativo");			
+	}
+
+	public void ingresar(double ingreso) {
+		if (ingreso > 0) {
+			setSaldo (getSaldo() + ingreso);
+			return;
+		}
+		System.out.println("El ingreso debe ser mayor que cero");
+	}
+
+	public void sacarDinero(double cantidad) {
+		if (cantidad <= getSaldo()) {
+			setSaldo (getSaldo() - cantidad);
+			return;
+		}
+		System.out.println("Disponible hoy en cajeros : " + getSaldo());
+	}
 }
