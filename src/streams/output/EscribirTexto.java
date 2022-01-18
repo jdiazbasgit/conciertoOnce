a4f849c144ede95fe93fe74b3da1b2a6cd8ae2e1
javier <javer@javier.com>
2022-01-18 14:12:52 +0100

    a

--- src/streams/output/EscribirTexto.java
+++ src/streams/output/EscribirTexto.java
@@ -2,23 +2,25 @@ package streams.output;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class EscribirTexto {
 
 	public static void main(String[] args) {
-		
-		//outputStream
-		// writer
-		
-		try {
-			FileOutputStream fileOutputStream= new FileOutputStream("prueba.txt");
-			PrintWriter printWriter= new PrintWriter(fileOutputStream);
+
+		try (FileOutputStream fileOutputStream = new FileOutputStream("src/prueba.txt", true);
+				PrintWriter printWriter = new PrintWriter(fileOutputStream,true);) {
+
 			printWriter.println("esto es una linea de texto");
 			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 
 	}
