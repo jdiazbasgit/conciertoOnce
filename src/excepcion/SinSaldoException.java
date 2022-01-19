f0e89d2a38225e063c708d5881d2f13a3b553842
Your Name <you@example.com>
2022-01-19 09:13:54 +0100

    excepciones banco

--- src/excepcion/SinSaldoException.java
+++ src/excepcion/SinSaldoException.java
@@ -3,4 +3,14 @@ package excepcion;
 @SuppressWarnings("serial")
 public class SinSaldoException extends Exception {
 
+	public SinSaldoException(String message) {
+		super(message);
+		
+	}
+
+	public SinSaldoException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
 }
