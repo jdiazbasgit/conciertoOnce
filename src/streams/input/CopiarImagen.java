1d1ac8e07caf7b4d49893e2fe32566f1d62a3a3d
Antonio Peinado Sevilla <a.peinado.sevilla@gmail.com>
2022-01-20 11:26:32 +0100

    pruebas

--- src/streams/input/CopiarImagen.java
+++ src/streams/input/CopiarImagen.java
@@ -6,21 +6,21 @@ import java.io.FileOutputStream;
 public class CopiarImagen {
 
 	public static void main(String[] args) {
-		try(
-				FileInputStream fileInputStream= new FileInputStream("descarga.jpg");
-				FileOutputStream fileOutputStream= new FileOutputStream("camion.jpg");
-				){
-			
-			byte[] b= new byte[fileInputStream.available()];
+
+		try (FileInputStream fileInputStream = new FileInputStream("descarga.jpg");
+				FileOutputStream fileOutputStream = new FileOutputStream("camion.jpg");) {
+
+			byte[] b = new byte[fileInputStream.available()];
 			fileInputStream.read(b);
-			for(int i=0;i<b.length;i++) {
+			for (int i = 0; i < b.length; i++) {
 				System.out.println(b[i]);
 				fileOutputStream.write(b[i]);
+
 			}
-			//fileOutputStream.write(b);
+			fileOutputStream.write(b);
 			fileOutputStream.flush();
 		} catch (Exception e) {
-			// TODO: handle exception
+
 		}
 
 	}
