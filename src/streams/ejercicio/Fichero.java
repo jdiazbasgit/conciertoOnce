924971329050d536b543280afd03b956cb1c53b0
Your Name <you@example.com>
2022-01-20 12:16:54 +0100

    Inicio ejercicio ficheros

--- src/streams/ejercicio/Fichero.java
+++ src/streams/ejercicio/Fichero.java
@@ -1,12 +1,33 @@
 package streams.ejercicio;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Fichero {
 
 	public static void main(String[] args) {
-		
-		
-		
 
+	}
+
+	
+
+	public static void menu() {
+
+		System.out.println("Menu Fichero:");
+		System.out.println("=============");
+		System.out.println("1.- Crear Fichero");
+		System.out.println("2.- Leer Fichero");
+		System.out.println("4.- Terminar");
+
+	}
+	private static String leerTeclado() {
+
+		try {
+			return new BufferedReader(new InputStreamReader(System.in)).readLine();
+		} catch (IOException e) {
+			return null;
+		}
 	}
 
 }
