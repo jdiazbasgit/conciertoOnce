f8fb4eea5a016d41e64ce5b2b015c801d55950d9
javier <javer@javier.com>
2022-01-25 14:06:45 +0100

    bolas

--- src/ventanas/VentanaBolas.java
+++ src/ventanas/VentanaBolas.java
@@ -5,43 +5,45 @@ import java.awt.Graphics;
 import java.awt.Image;
 
 public class VentanaBolas extends Frame {
-	
+
 	public VentanaBolas() {
-		this.setSize(1500,1000);
-		
+		this.setSize(500, 500);
+
 	}
 
 	@Override
 	public void paint(Graphics g) {
-		
-		Image imagen=this.createImage(1000,1000);
-		Graphics externo=imagen.getGraphics();
-		
-		while(true) {
-			externo.clearRect(0,0,1000,1000);
-			if(posicion<0 || posicion>950)
-				sentido=sentido*-1;
+
+		Image imagen = this.createImage(2000, 2000);
+		Graphics externo = imagen.getGraphics();
+		Bola bola = new Bola(200, 200, 1, 1, 2, 1, 50);
+		while (true) {
+			externo.clearRect(0, 0,this.getWidth() , this.getHeight());
 			
-			posicion=posicion+(incremento*sentido);
-			externo.fillOval(posicion,500,50,50);
-			g.drawImage(imagen,0,0,1000,1000, this);
+
+			if (bola.getPosicionX() < 0 || bola.getPosicionX() > this.getWidth()-bola.getDimension())
+				bola.setSentidoX(bola.getSentidoX() * -1);
+			
+			if (bola.getPosicionY() < 5 || bola.getPosicionY() > this.getHeight()-bola.getDimension())
+				bola.setSentidoY(bola.getSentidoY() * -1);
+
+			bola.setPosicionX(bola.getPosicionX() + (bola.getIncrementoX() * bola.getSentidoX()));
+			bola.setPosicionY(bola.getPosicionY() + (bola.getIncrementoY() * bola.getSentidoY()));
+			externo.fillOval(bola.getPosicionX(), bola.getPosicionY(), bola.getDimension(), bola.getDimension());
+			g.drawImage(imagen, 0, 0, 2000, 2000, this);
 			try {
-				Thread.sleep(2);
+				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void update(Graphics g) {
 		paint(g);
 	}
-	
-	
-	
-	
 
 }
--- src/ventanas/VerVentanaBolas.java
+++ src/ventanas/VerVentanaBolas.java
@@ -4,6 +4,7 @@ public class VerVentanaBolas {
 
 	public static void main(String[] args) {
 		VentanaBolas ventanaBolas= new VentanaBolas();
+		
 		ventanaBolas.setVisible(true);
 	}
 
