d5d6dd408eef342f170319f5e3a935ac0ec20038
Your Name <you@example.com>
2022-01-31 13:42:46 +0100

    mi rama de trenes

---
+++ src/trenes/EjemploSet.java
@@ -0,0 +1,22 @@
+package trenes;
+
+
+public class EjemploSet  {
+
+	public static void main(String[] args) {
+
+		Tren t1 = new Tren("Madrid", "Barcelona", 180);
+		Tren t2 = new Tren("Zaragoza", "Barcelona", 235);
+		Tren t3 = new Tren("Madrid", "Sevilla", 120);
+		Tren t4 = new Tren("Madrid", "Sevilla", 120);
+		Tren t5 = new Tren("Madrid", "Sevilla", 120);
+		Tren t6 = new Tren("Madrid", "Sevilla", 120);
+
+
+	}
+
+	
+		
+}
+
+
