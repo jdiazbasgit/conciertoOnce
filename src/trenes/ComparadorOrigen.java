0d1c8f6627a413b835a45778180f2ab9180d2986
javier <javer@javier.com>
2022-02-01 14:25:07 +0100

    comparator

---
+++ src/trenes/ComparadorOrigen.java
@@ -0,0 +1,13 @@
+package trenes;
+
+import java.util.Comparator;
+
+public class ComparadorOrigen implements Comparator<Tren> {
+
+	@Override
+	public int compare(Tren o1, Tren o2) {
+		// TODO Auto-generated method stub
+		return o1.getOrigen().compareTo(o2.getOrigen());
+	}
+
+}
