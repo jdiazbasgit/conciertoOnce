40c3726f14dddcafbfec5cd174b1466715681fad
Your Name <you@example.com>
2022-02-01 11:55:04 +0100

    cambios default

--- src/concierto/instrumentos/InstrumentoInterface.java
+++ src/concierto/instrumentos/InstrumentoInterface.java
@@ -5,6 +5,7 @@ import concierto.excepciones.SinSonidoException;
 
 public interface InstrumentoInterface {
 
+
 	public String sonar() throws SinSonidoException;
 
 	public InstrumentoBean getInstrumentoBean1();
