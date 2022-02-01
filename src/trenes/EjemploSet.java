100617eeb64d54035a317251931f7535ba723f5d
Your Name <you@example.com>
2022-02-01 09:05:00 +0100

    EjemploSet terminado

--- src/trenes/EjemploSet.java
+++ src/trenes/EjemploSet.java
@@ -1,5 +1,7 @@
 package trenes;
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class EjemploSet  {
 
@@ -11,6 +13,17 @@ public class EjemploSet  {
 		Tren t4 = new Tren("Madrid", "Sevilla", 120);
 		Tren t5 = new Tren("Madrid", "Sevilla", 120);
 		Tren t6 = new Tren("Madrid", "Sevilla", 120);
+		
+		 Set<Tren> conjunto = new HashSet<Tren>(); 
+		 conjunto.add(t1);
+		 conjunto.add(t2);
+		 conjunto.add(t3);
+		 conjunto.add(t4);
+		 conjunto.add(t5);
+		 conjunto.add(t6);
+		 for(Tren s : conjunto) {
+		     System.out.println(s);
+		 }
 
 
 	}
