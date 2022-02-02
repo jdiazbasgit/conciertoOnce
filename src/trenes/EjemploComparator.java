74f0e46896a34425804bfcd1c4884af7e4f2963c
CURSO-FULLSTACK-JAVA <CURSO-FULLSTACK-JAVA@DESKTOP-CF5VE98.mshome.net>
2022-02-02 12:41:59 +0100

    bien al completo

--- src/trenes/EjemploComparator.java
+++ src/trenes/EjemploComparator.java
@@ -1,8 +1,5 @@
 package trenes;
-import.trenes.tren;
-import trenes.ComparadorDestino;
-import trenes.ComparadorOrigen;
-import trenes.ComparadorPasajeros;
+
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -11,23 +8,18 @@ public class EjemploComparator {
 
 	public static void main(String[] args) {
 
-	
+		Tren t1 = new Tren("Madrid", "Barcelona", 180);
+		Tren t2 = new Tren("Zaragoza", "Barcelona", 235);
+		Tren t3 = new Tren("Madrid", "Sevilla", 520);
+		Tren t4 = new Tren("Madrid", "Oviedo", 720);
+		Tren t5 = new Tren("Madrid", "Cadiz", 620);
+		Tren t6 = new Tren("Madrid", "Almeria", 820);
 
-	Tren t1 = new Tren("Madrid", "Barcelona", 180);
-	Tren t2 = new Tren("Zaragoza", "Barcelona", 235);
-	Tren t3 = new Tren("Madrid", "Sevilla", 520);
-	Tren t4 = new Tren("Madrid", "Oviedo", 720);
-	Tren t5 = new Tren("Madrid", "Cadiz", 620);
-	Tren t6 = new Tren("Madrid", "Almeria", 820);
+		TreeSet<Tren> trenesJuntos = null;
 
-	
-	
-	Set<Tren> trenesJuntos = juntarTrenes(t1, t2, t3, t4, t5, t6);
-		
-	
-		int tipo;
+		int tipo = 0;
 		do {
-            System.out.println("*****************************************************");
+			System.out.println("*****************************************************");
 			System.out.println("Introduce 1 para Comparar por origen");
 			System.out.println("Introduce 2 para Comparar por destino");
 			System.out.println("Introduce 3 para Comparar por pasajeros");
@@ -35,23 +27,29 @@ public class EjemploComparator {
 			System.out.println("*****************************************************");
 			System.out.println("Teclea opcion: ");
 			System.out.println("****************************************************");
-           
-			try (Scanner reader = new Scanner(System.in)) {
+
+			try {
+				Scanner reader = new Scanner(System.in);
 				tipo = reader.nextInt();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 
 			switch (tipo) {
 			case 1:
 				trenesJuntos = new TreeSet<Tren>(new ComparadorOrigen());
-                listaTrenes(trenesJuntos);
+				juntarTrenes(trenesJuntos, t1, t2, t3, t4, t5, t6);
+				listaTrenes(trenesJuntos);
 				break;
 			case 2:
 				trenesJuntos = new TreeSet<Tren>(new ComparadorDestino());
-				 listaTrenes(trenesJuntos);
+				juntarTrenes(trenesJuntos, t1, t2, t3, t4, t5, t6);
+				listaTrenes(trenesJuntos);
 				break;
 			case 3:
 				trenesJuntos = new TreeSet<Tren>(new ComparadorPasajeros());
-				 listaTrenes(trenesJuntos);
+				juntarTrenes(trenesJuntos, t1, t2, t3, t4, t5, t6);
+				listaTrenes(trenesJuntos);
 				break;
 			case 0:
 				System.exit(0);
@@ -62,17 +60,17 @@ public class EjemploComparator {
 
 		} while (tipo != 0);
 	}
-	private static Set<Tren> juntarTrenes(Tren... trenes) {
-		
-		Set<Tren> trenesList = null;
-		for (int i = 0; i < trenes.length; i++)
-			if(trenesList != null)
-			trenesList.add(trenes[i]);
 
-		return trenesList;
+	private static void juntarTrenes(TreeSet<Tren> lista, Tren... trenes) {
+
+		for (int i = 0; i < trenes.length; i++)
+
+			lista.add(trenes[i]);
+
 	}
+
 	private static void listaTrenes(TreeSet<Tren> trenes) {
 		for (Tren tren : trenes)
-		System.out.println(tren.toString());
+			System.out.println(tren.toString());
 	}
 }
