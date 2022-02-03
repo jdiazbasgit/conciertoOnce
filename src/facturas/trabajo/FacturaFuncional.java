782b43d2703cc65dbace9741a6c71873438d3763
JuanVega <juanv@LegionJuan>
2022-02-04 00:32:01 +0100

    fd

---
+++ src/facturas/trabajo/FacturaFuncional.java
@@ -0,0 +1,26 @@
+package facturas.trabajo;
+
+import java.util.Set;
+
+import facturas.beans.Factura;
+import facturas.beans.Vendedor;
+
+public class FacturaFuncional {
+
+	public static void main(String[] args) {
+		Set<Vendedor> vendedores = cargaVendedores();
+		Set<Factura> facturas = cargaFacturas();
+
+	}
+
+	private static Set<Factura> cargaFacturas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Set<Vendedor> cargaVendedores() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
