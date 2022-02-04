f70c435a5df12464109370c3779f785c09853370
Antonio Peinado Sevilla <a.peinado.sevilla@gmail.com>
2022-02-04 10:23:37 +0100

    fecha numero y vendedor

--- src/facturas/trabajo/EjemploFactura.java
+++ src/facturas/trabajo/EjemploFactura.java
@@ -1,51 +1,59 @@
 package facturas.trabajo;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.HashSet;
+import java.util.Locale;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
-
 import facturas.beans.Factura;
 import facturas.beans.Vendedor;
 
 public class EjemploFactura {
 
 	public static void main(String[] args) {
-		
-		Set<Vendedor> vendedores=cargaVendedores();
-		Set<Factura> facturas=cargaFacturas();
-		for (Factura factura : facturas) {
-			
-			System.out.println(factura.getBaseImponible());
-			System.out.println(factura.getNumeroFactura());
-			System.out.println(factura.getFecha());
-			System.out.println(factura.getVendedor());
-		}
 
+		Set<Vendedor> vendedores = cargaVendedores();
+		Set<Factura> facturas = cargaFacturas(vendedores);
+		for (Factura factura : facturas) {
+			String fechaTexto = factura.getFecha().get(Calendar.DAY_OF_MONTH) + " de "
+					+ factura.getFecha().getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()) + " de "
+					+ factura.getFecha().get(Calendar.YEAR);
+			System.out.println(
+					factura.getNumeroFactura() + " " + " " + fechaTexto + " " + " " + factura.getBaseImponible());
+
+		}
 	}
 
-	private static Set<Factura> cargaFacturas() {
-		Set<Factura> salida= new TreeSet<>((f1,f2)->f1.getFecha().compareTo(f2.getFecha()));
+	private static Set<Factura> cargaFacturas(Set<Vendedor> vendedores) {
+		TreeSet<Factura> salida = new TreeSet<>((f1, f2) -> f1.getFecha().compareTo(f2.getFecha()));
+		int numeroFactura = 1;
+		Random random = new Random();
+
+		for (Vendedor vendedor : vendedores) {
+
+			for (int i = 0; i < 10; i++) {
+
+				int importe = (int) (random.nextFloat() * 10000);
+				GregorianCalendar fecha = new GregorianCalendar();
+				fecha.add(Calendar.DAY_OF_MONTH, numeroFactura);
+				Factura factura = new Factura(importe, numeroFactura, fecha, vendedor);
+				salida.add(factura);
+				numeroFactura++;
+
+			}
+
+		}
 		return salida;
 	}
 
 	private static Set<Vendedor> cargaVendedores() {
-		Set<Vendedor> salida=new HashSet<>();
-		for(int i=0;i<20;i++) {
-			salida.add(new Vendedor("nombre"+i, "dni"+i, "9191919"+1));
-		}
-		return salida;
-	}
-	@SuppressWarnings("unused")
-	private static Set<Factura> crearFacturas(){
-		Set<Factura> salida = new HashSet<>();
+		Set<Vendedor> salida = new HashSet<>();
 		for (int i = 0; i < 20; i++) {
-			
-			salida.add(new Factura());
-			
+			salida.add(new Vendedor("nombre" + i, "dni" + i, "9191919" + 1));
 		}
-		
 		return salida;
-		
 	}
+
 }
