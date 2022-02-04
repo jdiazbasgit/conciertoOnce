3631ab052fa7498e8b53ecdcbdcd3b55a614bdac
Your Name <you@example.com>
2022-02-04 10:10:09 +0100

    CARRERA LOCA

--- src/facturas/trabajo/EjemploFactura.java
+++ src/facturas/trabajo/EjemploFactura.java
@@ -1,6 +1,10 @@
 package facturas.trabajo;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashSet;
+import java.util.Locale;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -12,13 +16,40 @@ public class EjemploFactura {
 	public static void main(String[] args) {
 		
 		Set<Vendedor> vendedores=cargaVendedores();
-		Set<Factura> facturas=cargaFacturas();
+		Set<Factura> facturas=cargaFacturas(vendedores);
+		for (Factura factura : facturas) {
+			String fechaTexto=factura.getFecha().get(Calendar.DAY_OF_MONTH)+ "de" +
+		factura.getFecha().getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())+ "de" +
+					factura.getFecha().get(Calendar.YEAR);
+			
+			System.out.println(factura.getNumeroFactura()+"-"+fechaTexto+"-"+factura.getBaseImponible());
+			
+		}
 
 	}
 
-	private static Set<Factura> cargaFacturas() {
-		Set<Factura> salida= new TreeSet<>((f1,f2)->f1.getFecha().compareTo(f2.getFecha()));
-		return null;
+	private static Set<Factura> cargaFacturas(Set<Vendedor> vendedores) {
+		TreeSet<Factura> salida= new TreeSet<>((f1,f2)->f1.getFecha().compareTo(f2.getFecha()));
+		int numeroFactura=1;
+		Random random = new Random();
+		for(int i=0; i<10; i++){
+		
+	}
+	
+	for (Vendedor vendedor: vendedores) {
+		for(int i=0; i<10; i++) {
+			int importe=(int)(random.nextFloat()*10000);
+			GregorianCalendar fecha = new GregorianCalendar();
+			fecha.add(Calendar.DAY_OF_MONTH, numeroFactura);
+			Factura factura= new Factura(importe, numeroFactura, fecha, vendedor);
+			salida.add(factura);
+			numeroFactura++;
+		}
+		
+			
+		}
+		
+		return salida;
 	}
 
 	
