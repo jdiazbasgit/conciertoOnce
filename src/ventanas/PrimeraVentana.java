3f75fb5c142fdfd8139b23466f736085289a7cba
javier <javer@javier.com>
2022-02-08 11:40:38 +0100

    a

--- src/ventanas/PrimeraVentana.java
+++ src/ventanas/PrimeraVentana.java
@@ -7,12 +7,12 @@ import java.awt.Graphics;
 
 @SuppressWarnings("serial")
 public class PrimeraVentana extends Frame {
-
+     
 	public PrimeraVentana(String titulo) {
 		super(titulo);
 		this.setSize(500, 500);
 		this.setLocation(100,100);
-		
+		this.setSize(500, 500);
 		Color color= new Color(50,80,120);
 		this.setBackground(color);
 	}
