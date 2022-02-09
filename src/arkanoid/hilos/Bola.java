c754ca4feff3d34e15aaaa0707aa628b0000900a
javier <javer@javier.com>
2022-02-09 14:24:48 +0100

    arkanoid

---
+++ src/arkanoid/hilos/Bola.java
@@ -0,0 +1,58 @@
+package arkanoid.hilos;
+
+import java.awt.geom.Rectangle2D;
+
+import arkanoid.ventanas.Bloque;
+import arkanoid.ventanas.VentanaArkanoid;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Bola extends Thread {
+
+	private int posicionX, posicionY, sentidoX, sentidoY, incrementoX, incrementoY, dimension;
+	private VentanaArkanoid ventanaArkanoid;
+
+	@Override
+	public void run() {
+
+		while (true) {
+
+			try {
+				if (getPosicionX() < 0 || (getPosicionX() + getDimension()) > getVentanaArkanoid().getWidth()) {
+					setSentidoX(getSentidoX() * -1);
+				}
+				if (getPosicionY() < 0 || (getPosicionY() + getDimension()) > getVentanaArkanoid().getHeight()) {
+					setSentidoY(getSentidoY() * -1);
+				}
+				getVentanaArkanoid().getCuadrados().forEach(c->{
+					if(miraChoque(c)) {
+						getVentanaArkanoid().getCuadrados().remove(c);
+						i
+						this.setSentidoX(getSentidoX()*-1);
+						this.setSentidoY(getSentidoY()*-1);
+						
+					}
+				});
+				this.setPosicionX(this.getPosicionX()+this.getIncrementoX()*this.getSentidoX());;
+				this.setPosicionY(this.getPosicionY()+this.getIncrementoY()*this.getSentidoY());;
+			Thread.sleep(1);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
+		}
+
+	}
+
+	private synchronized boolean miraChoque(Bloque bloque) {
+		Rectangle2D.Double miRectangulo = new Rectangle2D.Double(this.getPosicionX(), this.getPosicionY(),
+				this.getDimension(), this.getDimension());
+		return miRectangulo.intersects((double) bloque.getPosicionX(), (double) bloque.getPosicionY(),
+				(double) bloque.getAncho(), (double) bloque.getAlto());
+	}
+
+}
--- src/arkanoid/ventanas/VentanaArkanoid.java
+++ src/arkanoid/ventanas/VentanaArkanoid.java
@@ -4,50 +4,119 @@ import java.awt.Frame;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ForkJoinWorkerThread;
+
+import arkanoid.eventos.EventosMio;
+import arkanoid.eventos.GestorEventosAdapter;
+import arkanoid.hilos.Bola;
+import arkanoid.hilos.Pintor;
+
 import java.awt.Graphics;
 import java.awt.Image;
 import lombok.Data;
+import java.awt.geom.Rectangle2D;
 
 @Data
 public class VentanaArkanoid extends Frame {
 
 	private Set<Bloque> cuadrados;
-	int ancho;
-	int alto;
-	int golpes;
-	int numeroBloques;
+	private int ancho;
+	private int alto;
+	private int golpes;
+	private int numeroBloques;
 	private Graphics externo;
 	private Image imagen;
-	private boolean primeraVez=true;
-	
-	VentanaArkanoid(){
-		Properties properties= new Properties();
+	private boolean primeraVez = true;
+	private Bola bola;
+
+	VentanaArkanoid() {
+		Properties properties = new Properties();
 		try {
 			properties.load(new InputStreamReader(new FileInputStream("arkanoid.properties")));
 			setAlto(Integer.parseInt(properties.getProperty("alto")));
 			setAncho(Integer.parseInt(properties.getProperty("ancho")));
 			setNumeroBloques(Integer.parseInt(properties.getProperty("bloques")));
 			setGolpes(Integer.parseInt(properties.getProperty("golpes")));
+			System.out.println(getAlto());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		this.addWindowListener(new EventosMio(this));
+		this.addMouseListener(new EventosMio(this));
 	}
 
 	@Override
 	public void paint(Graphics g) {
-		if(isPrimeraVez()) {
+		if (isPrimeraVez()) {
 			setImagen(createImage(2000, 2000));
 			setExterno(getImagen().getGraphics());
+			cargaBloques();
+			Pintor pintor = new Pintor(this);
+			pintor.start();
 			setPrimeraVez(false);
 		}
-		
+		getExterno().clearRect(0,0,2000,2000);
+		for (Bloque bloque : cuadrados) {
+			getExterno().drawRect(bloque.getPosicionX(), bloque.getPosicionY(), bloque.getAncho(), bloque.getAlto());
+		}
+		if (getBola() != null)
+			getExterno().fillOval(getBola().getPosicionX(), getBola().getPosicionY(), getBola().getDimension(),
+					getBola().getDimension());
+		g.drawImage(getImagen(),0,0,this);
 		
 	}
 	
-	
-	
-	
+	@Override
+	public void update(Graphics g) {
+		paint(g);
+	}
+
+	public void cargaBloques() {
+		if (getCuadrados() == null) {
+			setCuadrados(new HashSet<>());
+			while (getCuadrados().size() < getNumeroBloques()) {
+
+				int superior = 100;
+				int inferior = this.getHeight() - 200;
+				getCuadrados().add(dameBloque(inferior, superior));
+			}
+		}
+
+	}
+
+	private Bloque dameBloque(int inferior, int superior) {
+		Bloque bloque = null;
+
+		boolean fin = false;
+		while (!fin) {
+			int aleatorioX = (int) (Math.random() * 10000);
+			if (aleatorioX > 0 && aleatorioX < this.getWidth() - getAncho()) {
+				int aleatorioY = (int) (Math.random() * 10000);
+				if (aleatorioY > superior && aleatorioY < inferior - getAlto()) {
+					fin = true;
+
+					for (Bloque b : cuadrados) {
+						Rectangle2D.Double r1 = new Rectangle2D.Double(aleatorioX, aleatorioY, getAncho(), getAlto());
+						if (r1.intersects(b.getPosicionX(), b.getPosicionY(), b.getAncho(), b.getAlto())) {
+							fin = false;
+							break;
+						}
+					}
+					if (fin) {
+						bloque = new Bloque(getAncho(), getAlto(), aleatorioX, aleatorioY, 0);
+
+					}
+
+				}
+			}
+		}
+
+		return bloque;
+	}
+
 }
