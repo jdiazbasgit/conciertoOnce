6749a9cf81f6951ed108fdafeab706f0d4f09ee7
Your Name <you@example.com>
2022-02-14 12:23:15 +0100

    Cambios Chat

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -1,18 +1,24 @@
 package ventanas;
 
-import lombok.Data;
-import java.awt.Panel;
-import java.awt.Label;
-import java.awt.TextField;
-import java.awt.TextArea;
 import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.BorderLayout;
+import java.awt.Frame;
+
+import lombok.Data;
 @Data
-public class Chat {
+public class Chat extends Frame{
 	private Panel pGlobal,pSuperior,pCentral,pInferior,pCentralIzquierda,pCentralIzquierdaSuperior,pCentralIzquierdaInferior;
 	private Label lNick, lUsuarios, lMensaje;
 	private Button bRegistrar,bEnviar;
 	private TextField tNick,tMensaje;
 	private TextArea taUsuarios,taMensajes;
+	
 	
 	
 	public Chat() {
@@ -28,12 +34,36 @@ public class Chat {
 		setLMensaje(new Label("MENSAJE:"));
 		setBEnviar(new Button("ENVIAR"));
 		setBRegistrar(new Button("REGISTRAR"));
-		setTNick(new TextField(80));
+		setTNick(new TextField(20));
 		setTMensaje(new TextField(80));
 		setTaMensajes(new TextArea());
 		setTaUsuarios(new TextArea());
 		
+		getPSuperior().setLayout(new FlowLayout());
+		getPSuperior().add(getLNick());
+		getPSuperior().add(getTNick());
+		getPSuperior().add(getBRegistrar());
+		getPSuperior().setBackground(Color.GRAY);
+
+		getPInferior().setLayout(new FlowLayout());
+		getPInferior().add(getLMensaje());
+		getPInferior().add(getTMensaje());
+		getPInferior().add(getBEnviar());
+		getPInferior().setBackground(Color.PINK);
 		
+		getPGlobal().setLayout(new BorderLayout());
+		getPGlobal().add(getPSuperior(),BorderLayout.NORTH);
+		getPGlobal().add(getPInferior(),BorderLayout.SOUTH);
+		
+		this.setLayout(new BorderLayout());
+		this.add(getPGlobal(),BorderLayout.CENTER);
+		
+		
+	}
+	public static void main(String[] args) {
+		Chat chat = new Chat();
+		chat.setSize(500,500);
+		chat.setVisible(true);
 		
 	}
 
