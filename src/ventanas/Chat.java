a58a1f254311ada8c1a32229a9215e2167e04865
Your Name <you@example.com>
2022-02-14 13:23:17 +0100

    ASERT

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -11,16 +11,17 @@ import java.awt.TextArea;
 import java.awt.TextField;
 
 import lombok.Data;
+
 @SuppressWarnings("serial")
 @Data
 public class Chat extends Frame {
-	private Panel pGlobal,pSuperior,pCentral,pInferior,pCentralIzquierda,pCentralIzquierdaSuperior,pCentralIzquierdaInferior;
+	private Panel pGlobal, pSuperior, pCentral, pInferior, pCentralIzquierda, pCentralIzquierdaSuperior,
+			pCentralIzquierdaInferior;
 	private Label lNick, lUsuarios, lMensaje;
-	private Button bRegistrar,bEnviar;
-	private TextField tNick,tMensaje;
-	private TextArea taUsuarios,taMensajes;
-	
-	
+	private Button bRegistrar, bEnviar;
+	private TextField tNick, tMensaje;
+	private TextArea taUsuarios, taMensajes;
+
 	public Chat() {
 		setPGlobal(new Panel());
 		setPSuperior(new Panel());
@@ -36,57 +37,50 @@ public class Chat extends Frame {
 		setBRegistrar(new Button("REGISTRAR"));
 		setTNick(new TextField(15));
 		setTMensaje(new TextField(80));
-		setTaMensajes(new TextArea(2,20));
+		setTaMensajes(new TextArea(2, 20));
 		setTaUsuarios(new TextArea());
-		
+
 		getPSuperior().setLayout(new FlowLayout());
 		getPSuperior().add(getLNick());
 		getPSuperior().add(getTNick());
 		getPSuperior().add(getBRegistrar());
 		getPSuperior().setBackground(Color.GRAY);
-		
-		
-		
+
 		getPInferior().setLayout(new FlowLayout());
 		getPInferior().add(getLMensaje());
 		getPInferior().add(getTMensaje());
 		getPInferior().add(getBEnviar());
 		getPInferior().setBackground(Color.PINK);
-		
-		getPCentralIzquierdaSuperior().setLayout(new FlowLayout());		
+
+		getPCentralIzquierdaSuperior().setLayout(new FlowLayout());
 		getPCentralIzquierdaSuperior().add(getLUsuarios());
-		
+
 		getPCentralIzquierdaInferior().setLayout(new BorderLayout());
-		getPCentralIzquierdaInferior().add(getTaUsuarios());
-		
+		getPCentralIzquierdaInferior().add(getTaUsuarios(),BorderLayout.CENTER);
+
 		getPCentralIzquierda().setLayout(new BorderLayout());
 		getPCentralIzquierda().add(getPCentralIzquierdaSuperior(), BorderLayout.NORTH);
 		getPCentralIzquierda().add(getPCentralIzquierdaInferior(), BorderLayout.CENTER);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		getPCentral().setLayout(new BorderLayout());
+		getPCentral().add(getPCentralIzquierda(), BorderLayout.WEST);
+		getPCentral().add(getTaMensajes(), BorderLayout.CENTER);
+
 		getPGlobal().setLayout(new BorderLayout());
-		getPGlobal().add(getPSuperior(),BorderLayout.NORTH);
-		getPGlobal().add(getPInferior(),BorderLayout.SOUTH);
-		
+		getPGlobal().add(getPSuperior(), BorderLayout.NORTH);
+		getPGlobal().add(getPInferior(), BorderLayout.SOUTH);
+		getPGlobal().add(getPCentral(), BorderLayout.CENTER);
+
 		this.setLayout(new BorderLayout());
-		this.add(getPGlobal(),BorderLayout.CENTER);
-	
-		
+		this.add(getPGlobal(), BorderLayout.CENTER);
+
 	}
-	
-	public static void main (String[] args) {
-		Chat chat= new Chat();
-		chat.setSize(500,500);
+
+	public static void main(String[] args) {
+		Chat chat = new Chat();
+		chat.setSize(500, 500);
 		chat.setVisible(true);
-		
+
 	}
 
 }
