a08a791c8e4f9d18751f58b7a6b666235c8fbb5a
Antonio Peinado Sevilla <a.peinado.sevilla@gmail.com>
2022-02-14 12:19:43 +0100

    comienzo ventana

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -4,12 +4,16 @@ import lombok.Data;
 import java.awt.Panel;
 import java.awt.Label;
 import java.awt.TextField;
+import javax.swing.JFrame;
 import java.awt.TextArea;
 import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.BorderLayout;
 
-
+@SuppressWarnings("serial")
 @Data
-public class Chat {
+public class Chat extends JFrame{
 	private Panel pGlobal,pSuperior,pCentral,pInferior,pCentralIzquierda,pCentralIzquierdaSuperior,pCentralIzquierdaInferior;
 	private Label lNick, lUsuarios, lMensaje;
 	private Button bRegistrar,bEnviar;
@@ -31,13 +35,38 @@ public class Chat {
 		setLMensaje(new Label("MENSAJE:"));
 		setBEnviar(new Button("ENVIAR"));
 		setBRegistrar(new Button("REGISTRAR"));
-		setTNick(new TextField(80));
+		setTNick(new TextField(20));
 		setTMensaje(new TextField(80));
 		setTaMensajes(new TextArea());
 		setTaUsuarios(new TextArea());
 		
+		getPSuperior().setLayout(new FlowLayout());
+		getPSuperior().add(getLNick());
+		getPSuperior().add(getTNick());
+		getPSuperior().add(getBRegistrar());
+		getPSuperior().setBackground(Color.GRAY);
+		
+		getPInferior().setLayout(new FlowLayout());
+		getPInferior().add(getLMensaje());
+		getPInferior().add(getTMensaje());
+		getPInferior().add(getBEnviar());
+		getPInferior().setBackground(Color.PINK);
+		
+		getPGlobal().setLayout(new BorderLayout());
+		getPGlobal().add(getPSuperior(),BorderLayout.NORTH);
+		getPGlobal().add(getPInferior(),BorderLayout.SOUTH);
+		
+		this.setLayout(new BorderLayout());
+		this.add(getPGlobal(),BorderLayout.CENTER);
 		
 		
+	}
+	
+	public static void main(String[] args) {
+		Chat chat= new Chat();
+		chat.setSize(500,500);
+		chat.setVisible(true);
+		chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
