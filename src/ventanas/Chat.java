4f30a294435529650da18a6be346dfbed1a41703
marbolas <marjedi@yahoo.es>
2022-02-14 13:50:19 +0100

    kdfdflkdflkdfd

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -66,7 +66,7 @@ public class Chat extends Frame{
 		getPCentral().add(getPCentralIzquierda(),BorderLayout.WEST);
 		getPCentral().add(getTaMensajes(),BorderLayout.CENTER);
 		
-		
+		//y seguimos
 	//MAS RAMAS
 		//De rama en rama
 		//cambios
