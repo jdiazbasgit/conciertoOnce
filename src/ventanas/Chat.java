a3145d2eb940a6bc45de09f72ccff82fee6e60cd
Your Name <you@example.com>
2022-02-14 13:02:57 +0100

    cambios 2

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -37,7 +37,7 @@ public class Chat extends Frame{
 		setTNick(new TextField(20));
 		setTMensaje(new TextField(80));
 		setTaMensajes(new TextArea());
-		setTaUsuarios(new TextArea());
+		setTaUsuarios(new TextArea(2,20));
 		
 		getPSuperior().setLayout(new FlowLayout());
 		getPSuperior().add(getLNick());
@@ -51,12 +51,30 @@ public class Chat extends Frame{
 		getPInferior().add(getBEnviar());
 		getPInferior().setBackground(Color.PINK);
 		
+		getPCentralIzquierdaSuperior().setLayout(new FlowLayout());
+		getPCentralIzquierdaSuperior().add(getLUsuarios());
+		
+		getPCentralIzquierdaInferior().setLayout(new BorderLayout());
+		getPCentralIzquierdaInferior().add(getTaUsuarios(),BorderLayout.CENTER);
+		
+		
+		getPCentralIzquierda().setLayout(new BorderLayout());
+		getPCentralIzquierda().add(getPCentralIzquierdaSuperior(),BorderLayout.NORTH);
+		getPCentralIzquierda().add(getPCentralIzquierdaInferior(),BorderLayout.CENTER);
+		
+		
+		getPCentral().setLayout(new BorderLayout());
+		getPCentral().add(getPCentralIzquierda(),BorderLayout.WEST);
+		getPCentral().add(getTaMensajes(),BorderLayout.CENTER);
+		
+		
 		getPGlobal().setLayout(new BorderLayout());
 		getPGlobal().add(getPSuperior(),BorderLayout.NORTH);
 		getPGlobal().add(getPInferior(),BorderLayout.SOUTH);
+		getPGlobal().add(getPCentral(),BorderLayout.CENTER);
 		
 		this.setLayout(new BorderLayout());
-		this.add(getPGlobal(),BorderLayout.CENTER);
+		
 		
 		
 	}
