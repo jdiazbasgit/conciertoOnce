af2ddc7741839e5c2240737fa13581d9b12aa81f
Sara Fullstack <Sara Fullstack@DESKTOP-G7EUESR>
2022-02-14 12:15:51 +0100

    cambios en ventana

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -1,19 +1,30 @@
 package ventanas;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.awt.Panel;
 import java.awt.Label;
 import java.awt.TextField;
 import java.awt.TextArea;
+import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Frame;
 @Data
-public class Chat {
+@EqualsAndHashCode(callSuper=false)
+public class Chat extends Frame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Panel pGlobal,pSuperior,pCentral,pInferior,pCentralIzquierda,pCentralIzquierdaSuperior,pCentralIzquierdaInferior;
 	private Label lNick, lUsuarios, lMensaje;
 	private Button bRegistrar,bEnviar;
 	private TextField tNick,tMensaje;
 	private TextArea taUsuarios,taMensajes;
-	//cambio cosas
+
 	
 	
 	public Chat() {
@@ -29,13 +40,40 @@ public class Chat {
 		setLMensaje(new Label("MENSAJE:"));
 		setBEnviar(new Button("ENVIAR"));
 		setBRegistrar(new Button("REGISTRAR"));
-		setTNick(new TextField(80));
+		setTNick(new TextField(20));
 		setTMensaje(new TextField(80));
 		setTaMensajes(new TextArea());
 		setTaUsuarios(new TextArea());
 		
+		getPSuperior().setLayout(new FlowLayout());
+		getPSuperior().add(getLNick());
+		getPSuperior().add(getTNick());
+		getPSuperior().add(getBRegistrar());
+		getPSuperior().setBackground(Color.LIGHT_GRAY);
+		
+		getPInferior().setLayout(new FlowLayout());
+		getPInferior().add(getLMensaje());
+		getPInferior().add(getTMensaje());
+		getPInferior().add(getBEnviar());
+		getPInferior().setBackground(Color.PINK);
 		
 		
+		getPGlobal().setLayout(new BorderLayout());
+		getPGlobal().add(getPSuperior(),BorderLayout.NORTH);
+		getPGlobal().add(getPInferior(),BorderLayout.SOUTH);
+		
+		
+		this.setLayout(new BorderLayout());
+		this.add(getPGlobal(),BorderLayout.CENTER);
+		
+		
+		
+	}
+
+	public static void main(String[]args) {
+		Chat chat=new Chat();
+		chat.setSize(500,500);
+		chat.setVisible(true);
 	}
 
 }
