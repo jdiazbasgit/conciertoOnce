9b47cfef6fa3833c9212c9805c41771bf850636b
Litriboxer <litriboxer@hotmail.com>
2022-02-15 10:40:33 +0100

    apertura de rama grupo5

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -20,70 +20,66 @@ public class Chat extends Frame{
 	private TextField tNick,tMensaje;
 	private TextArea taUsuarios,taMensajes;
 	
-	 //fgfdg
-
-	// soy MA
 
 	public Chat() {
-		setPGlobal(new Panel());
-		setPSuperior(new Panel());
-		setPCentral(new Panel());
-		setPInferior(new Panel());
-		setPCentralIzquierda(new Panel());
-		setPCentralIzquierdaSuperior(new Panel());
-		setPCentralIzquierdaInferior(new Panel());
-		setLNick(new Label("NICK:"));
-		setLUsuarios(new Label("USUARIOS"));
-		setLMensaje(new Label("MENSAJE:"));
-		setBEnviar(new Button("ENVIAR"));
-		setBRegistrar(new Button("REGISTRAR"));
-		setTNick(new TextField(20));
-		setTMensaje(new TextField(80));
-		setTaMensajes(new TextArea());
-		setTaUsuarios(new TextArea(2,20));
-		
-		getPSuperior().setLayout(new FlowLayout());
-		getPSuperior().add(getLNick());
-		getPSuperior().add(getTNick());
-		getPSuperior().add(getBRegistrar());
-		getPSuperior().setBackground(Color.GRAY);
-		
-		getPInferior().setLayout(new FlowLayout());
-		getPInferior().add(getLMensaje());
-		getPInferior().add(getTMensaje());
-		getPInferior().add(getBEnviar());
-		getPInferior().setBackground(Color.PINK);
-		
-		getPCentralIzquierdaSuperior().setLayout(new FlowLayout());
-		getPCentralIzquierdaSuperior().add(getLUsuarios());
-		
-		getPCentralIzquierdaInferior().setLayout(new BorderLayout());
-		getPCentralIzquierdaInferior().add(getTaUsuarios(),BorderLayout.CENTER);
-		
-		getPCentralIzquierda().setLayout(new BorderLayout());
-		getPCentralIzquierda().add(getPCentralIzquierdaSuperior(),BorderLayout.NORTH);
-		getPCentralIzquierda().add(getPCentralIzquierdaInferior(),BorderLayout.CENTER);
-		
-		getPCentral().setLayout(new BorderLayout());
-		getPCentral().add(getPCentralIzquierda(),BorderLayout.WEST);
-		getPCentral().add(getTaMensajes(),BorderLayout.CENTER);
+		/*
+		 * setPGlobal(new Panel()); setPSuperior(new Panel()); setPCentral(new Panel());
+		 * setPInferior(new Panel()); setPCentralIzquierda(new Panel());
+		 * setPCentralIzquierdaSuperior(new Panel()); setPCentralIzquierdaInferior(new
+		 * Panel()); setLNick(new Label("NICK:")); setLUsuarios(new Label("USUARIOS"));
+		 * setLMensaje(new Label("MENSAJE:")); setBEnviar(new Button("ENVIAR"));
+		 * setBRegistrar(new Button("REGISTRAR")); setTNick(new TextField(20));
+		 * setTMensaje(new TextField(80)); setTaMensajes(new TextArea());
+		 * setTaUsuarios(new TextArea(2,20));
+		 */
 		
 		
-		//cambios
-		//soy mar
+		/*
+		 * getPSuperior().setLayout(new FlowLayout()); getPSuperior().add(getLNick());
+		 * getPSuperior().add(getTNick()); getPSuperior().add(getBRegistrar());
+		 * getPSuperior().setBackground(Color.GRAY);
+		 * 
+		 * getPInferior().setLayout(new FlowLayout());
+		 * getPInferior().add(getLMensaje()); getPInferior().add(getTMensaje());
+		 * getPInferior().add(getBEnviar()); getPInferior().setBackground(Color.PINK);
+		 * 
+		 * getPCentralIzquierdaSuperior().setLayout(new FlowLayout());
+		 * getPCentralIzquierdaSuperior().add(getLUsuarios());
+		 * 
+		 * getPCentralIzquierdaInferior().setLayout(new BorderLayout());
+		 * getPCentralIzquierdaInferior().add(getTaUsuarios(),BorderLayout.CENTER);
+		 * 
+		 * getPCentralIzquierda().setLayout(new BorderLayout());
+		 * getPCentralIzquierda().add(getPCentralIzquierdaSuperior(),BorderLayout.NORTH)
+		 * ;
+		 * getPCentralIzquierda().add(getPCentralIzquierdaInferior(),BorderLayout.CENTER
+		 * );
+		 * 
+		 * getPCentral().setLayout(new BorderLayout());
+		 * getPCentral().add(getPCentralIzquierda(),BorderLayout.WEST);
+		 * getPCentral().add(getTaMensajes(),BorderLayout.CENTER);
+		 * 
+		 * 
+		 * 
+		 * getPGlobal().setLayout(new BorderLayout());
+		 * getPGlobal().add(getPSuperior(),BorderLayout.NORTH);
+		 * getPGlobal().add(getPInferior(),BorderLayout.SOUTH);
+		 * getPGlobal().add(getPCentral(),BorderLayout.CENTER);
+		 * 
+		 * this.setLayout(new BorderLayout());
+		 * this.add(getPGlobal(),BorderLayout.CENTER);
+		 */
 		
 		
-		
-		getPGlobal().setLayout(new BorderLayout());
-		getPGlobal().add(getPSuperior(),BorderLayout.NORTH);
-		getPGlobal().add(getPInferior(),BorderLayout.SOUTH);
-		getPGlobal().add(getPCentral(),BorderLayout.CENTER);
-		 
-		this.setLayout(new BorderLayout());
-		this.add(getPGlobal(),BorderLayout.CENTER);
-		
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints gb1 = new GridBagConstraints(0, 0, 1,1,1,1, GridBagConstraints.NORTHEAST, GridBagConstraints.BOTH,
+				new Insets(0, 0, 0, 0), 0, 0);
+		panel.add(b1, gb1);
 		
 	}
+	
+	
+
 	
 	public static void main(String[] args) {
 		Chat chat= new Chat();
