8a1b580792a54dda0b1908129791250fe358a9f2
JuanVega <juanv@LegionJuan>
2022-02-15 18:27:35 +0100

    Para empezar

---
+++ src/ventanas/GestorVentanaChatAdapter.java
@@ -0,0 +1,44 @@
+package ventanas;
+
+import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+@SuppressWarnings("serial")
+public class GestorVentanaChatAdapter extends Frame implements WindowListener {
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+
+	}
+
+}
