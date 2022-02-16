ed8ed2a6788ec7755b22ac1a7255f70d71ab7554
Litriboxer <litriboxer@hotmail.com>
2022-02-16 14:01:28 +0100

    Reconfiguración peticiones Javier

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -1,6 +1,7 @@
 package ventanas;
 
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -14,7 +15,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @SuppressWarnings("serial")
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 @Data
 public class Chat extends Frame {
 
@@ -22,6 +23,7 @@ public class Chat extends Frame {
 	private Button bRegistrar, bEnviar;
 	private TextField tNick, tMensaje;
 	private TextArea taUsuarios, taMensajes;
+	private Color color;
 
 	public Chat() {
 		Panel panel = new Panel();
@@ -33,21 +35,23 @@ public class Chat extends Frame {
 		setBEnviar(new Button("ENVIAR"));
 		setBRegistrar(new Button("REGISTRAR"));
 		setTNick(new TextField(50));
-		setTMensaje(new TextField(80));
+		setTMensaje(new TextField(100));
 		setTaMensajes(new TextArea());
 		setTaUsuarios(new TextArea(2, 20));
 
 		panel.setLayout(new GridBagLayout());
+		Color color = new Color(232, 241, 242);
+		setBackground(color);
 
-		GridBagConstraints gbLNick = new GridBagConstraints(0, 0, 5, 1, 0,0.05, GridBagConstraints.EAST,
+		GridBagConstraints gbLNick = new GridBagConstraints(0, 0, 6, 1, 1, 0.1,  GridBagConstraints.EAST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		panel.add(getLNick(), gbLNick);
 
-		GridBagConstraints gbTNick= new GridBagConstraints(5, 0, 4, 1, 0.4, 0, GridBagConstraints.WEST,
-				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
+		GridBagConstraints gbTNick = new GridBagConstraints(6, 0, 1, 1, 0, 0, GridBagConstraints.WEST,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0);
 		panel.add(getTNick(), gbTNick);
 
-		GridBagConstraints gbBRegistrar = new GridBagConstraints(9, 0, 2, 1, 0.4, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbBRegistrar = new GridBagConstraints(7, 0, 3, 1, 0.8, 0, GridBagConstraints.WEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		panel.add(getBRegistrar(), gbBRegistrar);
 
@@ -55,23 +59,23 @@ public class Chat extends Frame {
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		panel.add(getLUsuarios(), gbLUsuarios);
 
-		GridBagConstraints gbTaUsuarios = new GridBagConstraints(0, 2, 3, 1, 0, 0.4, GridBagConstraints.CENTER,
+		GridBagConstraints gbTaUsuarios = new GridBagConstraints(0, 2, 3, 6, 0, 1.8, GridBagConstraints.CENTER,
 				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
 		panel.add(getTaUsuarios(), gbTaUsuarios);
 
-		GridBagConstraints gbTaMensajes = new GridBagConstraints(4, 1, 7, 8, 0, 0, GridBagConstraints.CENTER,
-				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
+		GridBagConstraints gbTaMensajes = new GridBagConstraints(4, 1, 7, 7, 0, 0, GridBagConstraints.CENTER,
+				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0 ), 0, 0);
 		panel.add(getTaMensajes(), gbTaMensajes);
 
-		GridBagConstraints gbLMensaje = new GridBagConstraints(0, 9, 8, 1, 0, 0.05, GridBagConstraints.EAST,
+		GridBagConstraints gbLMensaje = new GridBagConstraints(0, 9, 5, 1, 0.1, 0.1, GridBagConstraints.EAST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		panel.add(getLMensaje(), gbLMensaje);
 
-		GridBagConstraints gbTMensaje = new GridBagConstraints(8, 9, 1, 1, 0, 0, GridBagConstraints.WEST,
-				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
+		GridBagConstraints gbTMensaje = new GridBagConstraints(5, 9, 3, 1, 0, 0, GridBagConstraints.CENTER,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0);
 		panel.add(getTMensaje(), gbTMensaje);
 
-		GridBagConstraints gbBEnviar = new GridBagConstraints(9, 9, 1, 1, 0, 0, GridBagConstraints.WEST,
+		GridBagConstraints gbBEnviar = new GridBagConstraints(8, 9, 3, 1, 0.8, 0, GridBagConstraints.WEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		panel.add(getBEnviar(), gbBEnviar);
 
