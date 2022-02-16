3762e5adea30f09a6447512d8720e840340cc88e
JuanVega <juanv@LegionJuan>
2022-02-16 19:10:20 +0100

    Pendiente de cambiar tamaño columna izquierda

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -26,8 +26,8 @@ public class Chat extends GestorVentanaChatAdapter {
 	private Button bRegistrar, bEnviar;
 	private TextField tNick, tMensaje;
 	private TextArea taUsuarios, taMensajes;
-	private Color color = new Color(0, 150, 175);
-	Font fuente = new Font("Arial", Font.ITALIC, 15);
+	private Color color = new Color(240, 240, 240);
+	Font fuente = new Font("Helvetica", Font.ITALIC, 13);
 
 	public Chat() {
 		setPGlobal(new Panel());
@@ -36,8 +36,8 @@ public class Chat extends GestorVentanaChatAdapter {
 		setLMensaje(new Label("MENSAJE:"));
 		setBEnviar(new Button("ENVIAR"));
 		setBRegistrar(new Button("REGISTRAR"));
-		setTNick(new TextField());
-		setTMensaje(new TextField(80));
+		setTNick(new TextField(20));
+		setTMensaje(new TextField(300));
 		setTaMensajes(new TextArea());
 		setTaUsuarios(new TextArea(20, 1));
 
@@ -48,37 +48,37 @@ public class Chat extends GestorVentanaChatAdapter {
 
 //Superior
 
-		GridBagConstraints gbLNick = new GridBagConstraints(0, 0, 33, 20, 0, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbLNick = new GridBagConstraints(0, 0, 33, 20, 0, 0, GridBagConstraints.EAST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getLNick(), gbLNick);
-		GridBagConstraints gbTNick = new GridBagConstraints(33, 0, 33, 20, 1, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbTNick = new GridBagConstraints(33, 0, 33, 20, 1, 0, GridBagConstraints.WEST,
 				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getTNick(), gbTNick);
-		GridBagConstraints gbBRegister = new GridBagConstraints(66, 0, 34, 20, 0, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbBRegister = new GridBagConstraints(66, 0, 34, 20, 0, 0, GridBagConstraints.WEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getBRegistrar(), gbBRegister);
 
 //Center
 
-		GridBagConstraints gbLUsuarios = new GridBagConstraints(0, 20, 20, 10, 0, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbLUsuarios = new GridBagConstraints(0, 20, 33, 10, 0, 0, GridBagConstraints.EAST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getLUsuarios(), gbLUsuarios);
-		GridBagConstraints gbaTaUsuarios = new GridBagConstraints(0, 30, 20, 50, 0, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbaTaUsuarios = new GridBagConstraints(0, 30, 33, 50, 0, 1, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getTaUsuarios(), gbaTaUsuarios);
-		GridBagConstraints gbaTaMensajes = new GridBagConstraints(20, 20, 80, 60, 0, 1, GridBagConstraints.CENTER,
+		GridBagConstraints gbaTaMensajes = new GridBagConstraints(33, 20, 67, 60, 0, 0, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getTaMensajes(), gbaTaMensajes);
 
 //Inferior
 
-		GridBagConstraints gbLMensaje = new GridBagConstraints(0, 80, 33, 20, 0, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbLMensaje = new GridBagConstraints(0, 80, 33, 20, 0, 0, GridBagConstraints.EAST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getLMensaje(), gbLMensaje);
-		GridBagConstraints gbTMensaje = new GridBagConstraints(33, 80, 33, 20, 0, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbTMensaje = new GridBagConstraints(33, 80, 33, 20, 0, 0, GridBagConstraints.WEST,
 				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getTMensaje(), gbTMensaje);
-		GridBagConstraints gbBEnviar = new GridBagConstraints(66, 80, 34, 20, 0, 0, GridBagConstraints.CENTER,
+		GridBagConstraints gbBEnviar = new GridBagConstraints(66, 80, 34, 20, 0, 0, GridBagConstraints.WEST,
 				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
 		getPGlobal().add(getBEnviar(), gbBEnviar);
 
