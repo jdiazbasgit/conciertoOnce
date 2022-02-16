309a5cd7c02da56a960053b2949911359911727c
Antonio Peinado Sevilla <a.peinado.sevilla@gmail.com>
2022-02-16 13:28:01 +0100

    TERMINADO

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -1,11 +1,12 @@
 package ventanas;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.awt.Panel;
 import java.awt.Label;
 import java.awt.TextField;
 import javax.swing.JFrame;
-import javax.swing.plaf.InsetsUIResource;
 
 import java.awt.TextArea;
 import java.awt.Button;
@@ -19,7 +20,8 @@ import java.awt.GridBagConstraints;
 
 @SuppressWarnings("serial")
 @Data
-public class Chat extends JFrame {
+@EqualsAndHashCode(callSuper=false)
+public class Chat extends JFrame  {
 	private Panel pGlobal, pSuperior, pCentral, pInferior, pCentralIzquierda, pCentralIzquierdaSuperior,
 			pCentralIzquierdaInferior;
 	private Label lNick, lUsuarios, lMensaje;
@@ -28,8 +30,8 @@ public class Chat extends JFrame {
 	private TextArea taUsuarios, taMensajes;
 
 	public Chat() {
-		setLocation(100, 100);
-		setTitle("Chat del equipo 3");
+		//setLocation(100, 100);
+		setTitle("   CHAT DEL EQUIPO 3 - ANNA Y ANTONIO");
 		setPGlobal(new Panel());
 		setPSuperior(new Panel());
 		setPCentral(new Panel());
@@ -42,51 +44,52 @@ public class Chat extends JFrame {
 		setLMensaje(new Label("MENSAJE:"));
 		setBEnviar(new Button("ENVIAR"));
 		setBRegistrar(new Button("REGISTRAR"));
-		setTNick(new TextField(40));
+		setTNick(new TextField(20));
 		setTMensaje(new TextField(80));
 		setTaMensajes(new TextArea());
-		setTaUsuarios(new TextArea(30, 20));
+		setTaUsuarios(new TextArea(20,2));
 		
 		
 		
 		
 		Panel panel = new Panel();
-		
+		panel.setBackground(Color.LIGHT_GRAY);
+		this.add(panel);
 		panel.setLayout(new GridBagLayout());
 		
-		GridBagConstraints gbclNick = new GridBagConstraints(0,0,2,1,1,1,GridBagConstraints.EAST,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+		GridBagConstraints gbclNick = new GridBagConstraints(0,0,2,1,2,1,GridBagConstraints.EAST,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
 		panel.add(lNick,gbclNick);
 		
-		GridBagConstraints gbctNick = new GridBagConstraints(2,0,1,1,2,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+		GridBagConstraints gbctNick = new GridBagConstraints(2,0,1,1,3,0,GridBagConstraints.CENTER,GridBagConstraints.HORIZONTAL,new Insets(0,0,0,0),0,0);
 		panel.add(tNick,gbctNick);
 		
-		GridBagConstraints gbcbRegistrar = new GridBagConstraints(3,0,1,1,1,1,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+		GridBagConstraints gbcbRegistrar = new GridBagConstraints(3,0,1,1,1,2,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(0,30,0,0),0,0);
 		panel.add(bRegistrar,gbcbRegistrar);
-		this.add(panel);
 		
-		GridBagConstraints gbctMensaje = new GridBagConstraints(2,3,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+		
+		GridBagConstraints gbctMensaje = new GridBagConstraints(2,3,1,1,3,0,GridBagConstraints.CENTER,GridBagConstraints.HORIZONTAL,new Insets(0,0,0,0),0,0);
 		panel.add(tMensaje,gbctMensaje);
 		
 		
 		
 		
-		GridBagConstraints gbclMensaje = new GridBagConstraints(0,3,1,1,1,1,GridBagConstraints.EAST,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+		GridBagConstraints gbclMensaje = new GridBagConstraints(1,3,1,1,0,0,GridBagConstraints.EAST,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
 		panel.add(lMensaje,gbclMensaje);
 		
-		GridBagConstraints gbctaMensajes = new GridBagConstraints(2,2,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.BOTH,new Insets(0,0,0,0),0,0);
+		GridBagConstraints gbctaMensajes = new GridBagConstraints(1,1,3,2,9,0,GridBagConstraints.WEST,GridBagConstraints.BOTH,new Insets(0,0,0,0),0,0);
 		panel.add(taMensajes,gbctaMensajes);
 		
 		
 		
-		GridBagConstraints gbcbEnviar = new GridBagConstraints(3,3,1,1,1,1,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+		GridBagConstraints gbcbEnviar = new GridBagConstraints(3,3,1,1,4,1,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(0,30,0,0),0,0);
 		panel.add(bEnviar,gbcbEnviar);
 		
 		
 		
-		GridBagConstraints gbclUsuarios = new GridBagConstraints(0,1,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+		GridBagConstraints gbclUsuarios = new GridBagConstraints(0,1,1,1,1,0,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
 		panel.add(lUsuarios,gbclUsuarios);
 		
-		GridBagConstraints gbctAUsuarios = new GridBagConstraints(0,2,1,1,2,2,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
+		GridBagConstraints gbctAUsuarios = new GridBagConstraints(0,2,1,1,0,7,GridBagConstraints.EAST,GridBagConstraints.BOTH,new Insets(0,0,0,0),0,0);
 		panel.add(taUsuarios,gbctAUsuarios);
 		
 		
@@ -156,7 +159,7 @@ public class Chat extends JFrame {
 
 	public static void main(String[] args) {
 		Chat chat = new Chat();
-		chat.setSize(1000, 700);
+		chat.setSize(1560, 840);
 		chat.setVisible(true);
 		chat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
