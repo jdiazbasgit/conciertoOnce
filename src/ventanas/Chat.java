e1d507b4bb731214e615bff9b681bebb2a4ed63e
JACINTO SANCHEZ MACIAS <jacinto_sanchez@hotmail.com>
2022-02-16 09:01:44 +0100

    bien incompleto

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -80,37 +80,38 @@ public class Chat extends Frame{
 		this.add(getPGlobal(),BorderLayout.CENTER);
 		
 */	
-		pGlobal.setLayout(new GridBagLayout());
+		getPGlobal().setLayout(new GridBagLayout());
+		this.add(pGlobal);
 		GridBagConstraints gb_LNick = new GridBagConstraints (0,0,2,1,1,1,GridBagConstraints.WEST,
 				GridBagConstraints.NONE, new Insets(0,0,0,0),0,0);
-		pGlobal.add(lNick,gb_LNick);
+		getPGlobal().add(lNick,gb_LNick);
 		GridBagConstraints gb_TNick = new GridBagConstraints (0,2,1,1,1,1,GridBagConstraints.CENTER,
 				GridBagConstraints.BOTH, new Insets(0,0,0,0),0,0);
-		pGlobal.add(tNick,gb_TNick);
+		getPGlobal().add(tNick,gb_TNick);
 
 		GridBagConstraints gb_BRegistrar= new GridBagConstraints (0,3,1,1,1,1,GridBagConstraints.EAST,
 				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		pGlobal.add(bRegistrar,gb_BRegistrar);
+		getPGlobal().add(bRegistrar,gb_BRegistrar);
 		
 		GridBagConstraints gb_LUsuarios = new GridBagConstraints (1,0,1,1,1,1,GridBagConstraints.WEST,
 				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		pGlobal.add(lUsuarios,gb_LUsuarios);
+		getPGlobal().add(lUsuarios,gb_LUsuarios);
 		
 		GridBagConstraints gb_TaUsuarios = new GridBagConstraints (2,0,1,1,1,1,GridBagConstraints.CENTER,
 				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		pGlobal.add(taUsuarios,gb_TaUsuarios);
+		getPGlobal().add(taUsuarios,gb_TaUsuarios);
 		
 		GridBagConstraints gb_LMensaje = new GridBagConstraints (3,0,1,1,1,1,GridBagConstraints.WEST,
 				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		pGlobal.add(lMensaje,gb_LMensaje);
+		getPGlobal().add(lMensaje,gb_LMensaje);
 		
 		GridBagConstraints gb_TMensaje = new GridBagConstraints (3,2,1,1,1,1,GridBagConstraints.CENTER,
 				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		pGlobal.add(tMensaje,gb_TMensaje);
+		getPGlobal().add(tMensaje,gb_TMensaje);
 		
 		GridBagConstraints gb_BEnviar = new GridBagConstraints (3,3,1,1,1,1,GridBagConstraints.EAST,
 				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		pGlobal.add(bEnviar,gb_BEnviar);
+		getPGlobal().add(bEnviar,gb_BEnviar);
 		
 		
 		
