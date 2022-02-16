9230081c323589bd2c02dacaf7217cb88528711c
JACINTO SANCHEZ MACIAS <jacinto_sanchez@hotmail.com>
2022-02-16 11:04:00 +0100

    FORMATO

--- src/ventanas/Chat.java
+++ src/ventanas/Chat.java
@@ -1,4 +1,5 @@
 package ventanas;
+
 import java.awt.Color;
 import java.awt.Button;
 import java.awt.Frame;
@@ -18,18 +19,18 @@ import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
- 
+
 @SuppressWarnings("serial")
 @Data
-public class Chat extends Frame{
-	private Panel pGlobal,pSuperior,pCentral,pInferior,pCentralIzquierda,pCentralIzquierdaSuperior,pCentralIzquierdaInferior;
+public class Chat extends Frame {
+	private Panel pGlobal, pSuperior, pCentral, pInferior, pCentralIzquierda, pCentralIzquierdaSuperior,
+			pCentralIzquierdaInferior;
 	private Label lNick, lUsuarios, lMensaje;
-	private Button bRegistrar,bEnviar;
-	private TextField tNick,tMensaje;
-	private TextArea taUsuarios,taMensajes;
-	//cambio cosas
-	
-	  
+	private Button bRegistrar, bEnviar;
+	private TextField tNick, tMensaje;
+	private TextArea taUsuarios, taMensajes;
+	// cambio cosas
+
 	/**
 	 * 
 	 */
@@ -49,16 +50,12 @@ public class Chat extends Frame{
 		setTNick(new TextField(20));
 		setTMensaje(new TextField(80));
 		setTaMensajes(new TextArea());
-		setTaUsuarios(new TextArea(2,20));
-		
+		setTaUsuarios(new TextArea(2, 20));
 
-	/*	getPSuperior().setLayout(new FlowLayout());
-		getPSuperior().add(getLNick());
-		getPSuperior().add(getTNick());
-		getPSuperior().add(getBRegistrar());
-		getPSuperior().setBackground(Color.GRAY);
-=======
 		/*
+		 * getPSuperior().setLayout(new FlowLayout()); getPSuperior().add(getLNick());
+		 * getPSuperior().add(getTNick()); getPSuperior().add(getBRegistrar());
+		 * getPSuperior().setBackground(Color.GRAY); ======= /*
 		 * getPSuperior().setLayout(new FlowLayout()); getPSuperior().add(getLNick());
 		 * getPSuperior().add(getTNick()); getPSuperior().add(getBRegistrar());
 		 * getPSuperior().setBackground(Color.GRAY);
@@ -93,58 +90,51 @@ public class Chat extends Frame{
 		 * this.setLayout(new BorderLayout());
 		 * this.add(getPGlobal(),BorderLayout.CENTER);
 		 */
-	
-		
-		
-		
-		
-		
+
 		getPGlobal().setLayout(new GridBagLayout());
 		this.add(pGlobal);
 		getPGlobal().setBackground(Color.PINK);
-		GridBagConstraints gb_LNick = new GridBagConstraints (0,0,2,1,1,1,GridBagConstraints.EAST,
-				GridBagConstraints.NONE, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(lNick,gb_LNick);
-		GridBagConstraints gb_TNick = new GridBagConstraints (2,0,1,1,1,1,GridBagConstraints.CENTER,
-				GridBagConstraints.HORIZONTAL, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(tNick,gb_TNick);
+		GridBagConstraints gb_LNick = new GridBagConstraints(0, 0, 2, 1, 1, 1, GridBagConstraints.EAST,
+				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(lNick, gb_LNick);
+		GridBagConstraints gb_TNick = new GridBagConstraints(2, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
+				GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(tNick, gb_TNick);
 
-		GridBagConstraints gb_BRegistrar= new GridBagConstraints (3,0,1,1,1,1,GridBagConstraints.CENTER,
-				GridBagConstraints.NONE, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(bRegistrar,gb_BRegistrar);
-		
-		GridBagConstraints gb_LUsuarios = new GridBagConstraints (0,1,1,1,0,0,GridBagConstraints.CENTER,
-				GridBagConstraints.NONE, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(lUsuarios,gb_LUsuarios);
-		
-		GridBagConstraints gb_TaUsuarios = new GridBagConstraints (0,2,1,1,0,2,GridBagConstraints.WEST,
-				GridBagConstraints.BOTH, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(taUsuarios,gb_TaUsuarios);
-		
-		GridBagConstraints gb_LMensaje = new GridBagConstraints (0,3,1,1,1,1,GridBagConstraints.CENTER,
-				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(lMensaje,gb_LMensaje);
-		
-		GridBagConstraints gb_TMensaje = new GridBagConstraints (2,3,1,1,1,1,GridBagConstraints.CENTER,
-				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(tMensaje,gb_TMensaje);
-		
-		GridBagConstraints gb_BEnviar = new GridBagConstraints (3,3,1,1,1,1,GridBagConstraints.CENTER,
-				GridBagConstraints.CENTER, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(bEnviar,gb_BEnviar);
-		
-		GridBagConstraints gb_TaMensajes = new GridBagConstraints (2,2,2,1,9,7,GridBagConstraints.CENTER,
-				GridBagConstraints.BOTH, new Insets(0,0,0,0),0,0);
-		getPGlobal().add(taMensajes,gb_TaMensajes);
-		
-		
+		GridBagConstraints gb_BRegistrar = new GridBagConstraints(3, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
+				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(bRegistrar, gb_BRegistrar);
+
+		GridBagConstraints gb_LUsuarios = new GridBagConstraints(0, 1, 1, 1, 0, 0, GridBagConstraints.CENTER,
+				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(lUsuarios, gb_LUsuarios);
+
+		GridBagConstraints gb_TaUsuarios = new GridBagConstraints(0, 2, 1, 1, 0, 2, GridBagConstraints.WEST,
+				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(taUsuarios, gb_TaUsuarios);
+
+		GridBagConstraints gb_LMensaje = new GridBagConstraints(0, 3, 1, 1, 1, 1, GridBagConstraints.CENTER,
+				GridBagConstraints.CENTER, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(lMensaje, gb_LMensaje);
+
+		GridBagConstraints gb_TMensaje = new GridBagConstraints(2, 3, 1, 1, 1, 1, GridBagConstraints.CENTER,
+				GridBagConstraints.CENTER, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(tMensaje, gb_TMensaje);
+
+		GridBagConstraints gb_BEnviar = new GridBagConstraints(3, 3, 1, 1, 1, 1, GridBagConstraints.CENTER,
+				GridBagConstraints.CENTER, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(bEnviar, gb_BEnviar);
+
+		GridBagConstraints gb_TaMensajes = new GridBagConstraints(2, 2, 2, 1, 9, 7, GridBagConstraints.CENTER,
+				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0);
+		getPGlobal().add(taMensajes, gb_TaMensajes);
+
 	}
-	
+
 	public static void main(String[] args) {
-		Chat chat= new Chat();
-		chat.setSize(500,500);
+		Chat chat = new Chat();
+		chat.setSize(500, 500);
 		chat.setVisible(true);
 	}
-
 
 }
