8e5d3ff553cded9ce82d587b85dd623d797446b2
Your Name <you@example.com>
2022-02-18 14:25:55 +0100

    añadido constructor y socket

--- src/ventanas/HiloEscuchaRegistroServidor.java
+++ src/ventanas/HiloEscuchaRegistroServidor.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class HiloEscuchaRegistroServidor extends Thread {
 	private Chat chat;
+	private HashMap hashMap;
 
 	public HiloEscuchaRegistroServidor(Chat chat) {
 		this.chat = chat;
