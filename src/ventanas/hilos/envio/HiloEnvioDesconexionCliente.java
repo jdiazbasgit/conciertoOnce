a22b8b0b1033ff22b8e38839b5e7eb56eab08e51
javier <javer@javier.com>
2022-02-21 13:28:03 +0100

    arrgelo de ip en envio

--- src/ventanas/hilos/envio/HiloEnvioDesconexionCliente.java
+++ src/ventanas/hilos/envio/HiloEnvioDesconexionCliente.java
@@ -8,13 +8,13 @@ import ventanas.Chat;
 public class HiloEnvioDesconexionCliente extends HiloEnvio {
 
 	public HiloEnvioDesconexionCliente(Chat chat, int puerto) {
-		super(chat, puerto);
+		super(chat, Chat.IP_SERVIDOR,Chat.PUERTO_DESCONEXION);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void hacerAlgo(Socket socket) throws IOException {
-		// TODO Auto-generated method stub
+		
 
 	}
 
