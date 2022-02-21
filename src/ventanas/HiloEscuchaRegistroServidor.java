e55e721666e20bc6b1cda0cdfc583c784fc4351c
Your Name <you@example.com>
2022-02-21 13:10:50 +0100

    cambios para mergear nuevo chat Javier

--- src/ventanas/HiloEscuchaRegistroServidor.java
+++ src/ventanas/HiloEscuchaRegistroServidor.java
@@ -5,43 +5,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.Key;
-import java.util.HashMap;
-import java.util.Map;
 
-public class HiloEscuchaRegistroServidor extends Thread{
+import lombok.Data;
+import ventanas.hilos.recepcion.HiloEscucha;
+@Data
+public class HiloEscuchaRegistroServidor extends HiloEscucha {
 	private Chat chat;
+	private int puerto;
 	
 	
-	public HiloEscuchaRegistroServidor() {
-		super();
-		// TODO Auto-generated constructor stub
-	}                                 //Constructor de clasr Padre
 	
+
+	public HiloEscuchaRegistroServidor(Chat chat, int puerto) {
+		super(chat, puerto);
+	}
+
+
+
 
 	@Override
-	public void run() {
-		ServerSocket serverSocket = null;
-		try {
-			serverSocket = new ServerSocket(5001);
-			while (true) {
-				Socket socketDeRecepcion = serverSocket.accept();
-				String direccionIp = socketDeRecepcion.getInetAddress().getHostAddress();
-				InputStreamReader inputStreamReader = new InputStreamReader(socketDeRecepcion.getInputStream());
-				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-				String nickDeUsuario = bufferedReader.readLine();
-
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				serverSocket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
+	public void hacerAlgo(Socket socket) throws IOException {
+		
+		
+		
 	}
 
 
