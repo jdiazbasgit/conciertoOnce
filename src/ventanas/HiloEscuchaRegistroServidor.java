fbb7fce5d65c82038c2075e024b2af0e8d857d25
Your Name <you@example.com>
2022-02-21 13:23:59 +0100

    Cambios antes de Mergeo con nueva clase chat javier

--- src/ventanas/HiloEscuchaRegistroServidor.java
+++ src/ventanas/HiloEscuchaRegistroServidor.java
@@ -3,38 +3,31 @@ package ventanas;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 import lombok.Data;
 import ventanas.hilos.recepcion.HiloEscucha;
+
 @Data
 public class HiloEscuchaRegistroServidor extends HiloEscucha {
 	private Chat chat;
 	private int puerto;
-	
-	
-	
 
 	public HiloEscuchaRegistroServidor(Chat chat, int puerto) {
 		super(chat, puerto);
 	}
 
-
-
-
 	@Override
 	public void hacerAlgo(Socket socket) throws IOException {
-		
-		
-		
-	}
+		getChat().getUsuarios().ifPresent(m -> {
+			String usuario = null;
+			try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+				usuario = bufferedReader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
+		});
 
-	
-
-
-	
-	
-
+	};
 }
