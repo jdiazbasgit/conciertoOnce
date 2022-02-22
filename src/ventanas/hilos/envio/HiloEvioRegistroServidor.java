0c9ec6d90cfcbe40e95a781fa8bf50de1a3a2549
Antonio Peinado Sevilla <a.peinado.sevilla@gmail.com>
2022-02-22 08:38:37 +0100

    enviar objeto

--- src/ventanas/hilos/envio/HiloEvioRegistroServidor.java
+++ src/ventanas/hilos/envio/HiloEvioRegistroServidor.java
@@ -1,42 +1,27 @@
 package ventanas.hilos.envio;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.net.Socket;
 
-public class HiloEvioRegistroServidor extends Thread {
+import ventanas.Chat;
+
+public class HiloEvioRegistroServidor extends HiloEnvio  {
+
+	
+	
+
+	public HiloEvioRegistroServidor(Chat chat,String ip, int puerto) {
+		super(chat, ip, puerto);	
+	}
+
+
 
 	@Override
-	public void run() {
-		Socket socket = null;
-
-		try  {
-			socket = new Socket("getIp", 5000);
-			
-				
-			
-			
-			
-			
-			
-			
-			//TODO recibir y mandar paquete con ip y nick
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		} finally {
-			try {
-				socket.close();
-			} catch (IOException e) {
-
-				e.printStackTrace();
-
-			}
-		}
-
+	public void hacerAlgo(Socket socket) throws IOException {
+		
+		ObjectOutputStream objectOutputStream= new ObjectOutputStream(socket.getOutputStream());
+		objectOutputStream.writeObject(socket);
+		objectOutputStream.flush();
 	}
 }
