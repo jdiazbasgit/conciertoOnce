f8ec5de1deca003e9fca5e0c40a327253e40952b
Your Name <you@example.com>
2022-02-22 10:19:18 +0100

    Cambios en grupo - terminado hilo envio registro cliente

---
+++ src/ventanas/trabajo/Cliente.java
@@ -0,0 +1,12 @@
+package ventanas.trabajo;
+
+import ventanas.hilos.envio.HiloCliente;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+		HiloCliente cliente = new HiloCliente();
+		cliente.start();
+	}
+
+}
