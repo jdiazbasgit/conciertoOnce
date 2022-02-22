9f4aaf3dda75c911eebf178848bb2aa9dc5b4534
Mar <maralmagromoreno@gmail.com>
2022-02-22 14:23:30 +0100

    Bis

---
+++ src/ventanas/eventos/ElQueSabeLoQueHayQueHacerConBotonMensaje.java
@@ -0,0 +1,32 @@
+package ventanas.eventos;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import ventanas.Chat;
+import ventanas.hilos.envio.HiloEnvioMensajeCliente;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ElQueSabeLoQueHayQueHacerConBotonMensaje implements ActionListener {
+
+	private Chat chat;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("ENVIAR") &&
+				!getChat().getTMensaje().getText().trim().equals("")) {
+
+			HiloEnvioMensajeCliente hiloEnvioMensaje = new HiloEnvioMensajeCliente(getChat(), Chat.IP_SERVIDOR,
+					Chat.PUERTO_ESCUCHA_REGISTRO_SERVIDOR);
+			hiloEnvioMensaje.start();
+
+		}
+
+	}
+
+}
