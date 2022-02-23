16f973f4a627e89dba00c90a96fce51fe312d366
Your Name <you@example.com>
2022-02-23 10:31:01 +0100

    HiloEnvioMensajeServidor terminado

--- src/ventanas/hilos/envio/HiloEnvioMensajesServidor.java
+++ src/ventanas/hilos/envio/HiloEnvioMensajesServidor.java
@@ -1,5 +1,36 @@
 package ventanas.hilos.envio;
 
-public class HiloEnvioMensajesServidor extends Thread {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import ventanas.trabajo.Chat;
+
+public class HiloEnvioMensajesServidor extends HiloEnvio {
+	
+	  private String texto; private String ip; private int puerto;
+	
+ 
+	public HiloEnvioMensajesServidor(String texto, String ip, int puerto) {
+		
+		
+		 this.texto = texto; this.ip = ip; this.puerto = puerto;
+		 
+	}
+
+	
+		
+	@Override
+	public void hacerAlgo(Socket socket) throws IOException {
+		DataOutputStream envioMensaje = new DataOutputStream(socket.getOutputStream());
+
+		envioMensaje(getChat().getTMensaje().getText());
+
+		/*
+		 * printWriter.println(getChat().getTNick().getText()); printWriter.flush();
+		 */
+	}
 
 }
