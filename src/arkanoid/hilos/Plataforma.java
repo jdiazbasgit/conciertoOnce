8cc7808c08b0a35af6a4354ccc3049a7d7a41d6c
JuanVega <juanvegarosa2@gmail.com>
2022-02-25 11:56:10 +0100

    Punto de inicio del nuevo arkanoid

---
+++ src/arkanoid/hilos/Plataforma.java
@@ -0,0 +1,16 @@
+package arkanoid.hilos;
+
+import javax.swing.event.MouseInputAdapter;
+
+public class Plataforma extends MouseInputAdapter implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+
+}
