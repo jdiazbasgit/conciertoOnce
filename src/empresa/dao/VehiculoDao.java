922832dcd24dd5e56b2d8d4be450c7d1edb0cf5d
Your Name <you@example.com>
2022-03-03 14:20:00 +0100

    dao sin resolver

--- src/empresa/dao/VehiculoDao.java
+++ src/empresa/dao/VehiculoDao.java
@@ -1,10 +1,13 @@
 package empresa.dao;
 
-import java.rmi.ConnectIOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JTable;
 
 import empresa.beans.Vehiculo;
 import lombok.Data;
@@ -15,6 +18,7 @@ import lombok.EqualsAndHashCode;
 public class VehiculoDao extends EmpresaDao {
 	
 	private Vehiculo vehiculo;
+	
 
 	public VehiculoDao(Vehiculo vehiculo) {
 		super();
@@ -23,8 +27,18 @@ public class VehiculoDao extends EmpresaDao {
 	
 	public  List<Vehiculo> dameVehiculos() {
 		List<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+		//JTable tablavehiculos = new Jtable(Vehiculo[1][3],String[3]);
+		//Lista<Vehiculos>listaVehiculos = new
 		try {
 			Connection conexion = dameConexion();
+			
+			
+			
+			Statement instruccion= conexion.createStatement();
+			ResultSet resultSet= instruccion.executeQuery("SELECT ID,MARCA,MODELO FROM VEHICULOS");
+			while(resultSet.next()) {
+				System.out.println(resultSet.getInt(1)+ " - "+resultSet.getString(2)+" - "+resultSet.getString(3));
+			}
 			
 		} catch (SQLException e)
 		{
@@ -34,6 +48,8 @@ public class VehiculoDao extends EmpresaDao {
 			return vehiculos;
 		}
 	}
+	
+	//public void grabarVehiculo() {
 	
 
 	
