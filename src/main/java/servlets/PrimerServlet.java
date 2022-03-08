a95d9d0e71f8995fca493a000d8d07e2ee9bc0ad
Your Name <you@example.com>
2022-03-08 14:22:19 +0100

    contador

--- src/main/java/servlets/PrimerServlet.java
+++ src/main/java/servlets/PrimerServlet.java
@@ -1,16 +1,22 @@
 package servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.Data;
+
+@Data
 /**
  * Servlet implementation class PrimerServlet
  */
 public class PrimerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	private int contador;
 
 	/**
 	 * Default constructor.
@@ -27,10 +33,12 @@ public class PrimerServlet extends HttpServlet {
 			throws ServletException, IOException {
 		String texto1 = request.getParameter("texto1");
 		String texto2 = request.getParameter("texto2");
-		String texto = texto1+texto2;
+		String texto = texto1 + texto2;
 		String salida = "<h1>Has escrito " + texto.toUpperCase() + " y tiene " + texto.length() + " letras</h1>";
 		response.getWriter().println(salida);
-		
+
+		setContador(getContador() + 1);
+		response.getWriter().println("<h1 style='color:#ff0000'>Eres el visitante numero: " + getContador() + "</h1>");
 
 	}
 
