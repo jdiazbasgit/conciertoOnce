faaa6a5e2a5481d2b622afacf35e355d7d2863f1
Your Name <you@example.com>
2022-03-09 09:47:06 +0100

    sesiones

--- src/main/java/servlets/PrimerServlet.java
+++ src/main/java/servlets/PrimerServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import lombok.Data;
 
@@ -38,8 +39,23 @@ public class PrimerServlet extends HttpServlet {
 		response.getWriter().println(salida);
 
 		setContador(getContador() + 1);
+
 		response.getWriter().println("<h1 style='color:#ff0000'>Eres el visitante numero: " + getContador() + "</h1>");
 
+		HttpSession session = request.getSession(true);
+
+		if (session.getAttribute("visitas") == null) {
+			// no has venido nunca
+			session.setAttribute("visitas", 1);
+			// se mete uno nuevo
+		} else {
+			int visitas = (int) session.getAttribute("visitas");
+			visitas++;
+			session.setAttribute("visitas", visitas);
+		}
+		response.getWriter().println(
+				"<h1 style='color:#00ff00'>Tu has venido a verme " 
+		+ session.getAttribute("visitas") + " veces</h1>");
 	}
 
 	/**
