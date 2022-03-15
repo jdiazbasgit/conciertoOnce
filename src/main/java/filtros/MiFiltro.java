e86dc340373d5951f0ca72577b563c9c7bb6eb0d
JuanVega <juanvegarosa2@gmail.com>
2022-03-15 12:07:07 +0100

    Ejercicio actualizado con filtros y listeners

---
+++ src/main/java/filtros/MiFiltro.java
@@ -0,0 +1,58 @@
+package filtros;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class MiFiltro
+ */
+public class MiFiltro implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public MiFiltro() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+//		Todo lo que hago aquí filtra a la ida
+		long tiempo1 = System.currentTimeMillis();
+
+		chain.doFilter(request, response);
+
+//		Todo lo que hago aquí filtra a la vuelta
+		long tiempo2 = System.currentTimeMillis();
+		HttpServletResponse resp = (HttpServletResponse) response;
+		resp.getWriter().println("<h1>El tiempo de carga es: " + (tiempo2 - tiempo1) + "</h1>");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/java/listeners/MiListener.java
@@ -0,0 +1,55 @@
+package listeners;
+
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class MiListener
+ *
+ */
+public class MiListener implements HttpSessionListener, HttpSessionAttributeListener {
+
+	@Override
+	public void attributeAdded(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void attributeRemoved(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void attributeReplaced(HttpSessionBindingEvent arg0) {
+		System.out.println(arg0.getSession().getAttribute("visitas"));
+
+	}
+
+	/**
+	 * Default constructor.
+	 */
+	public MiListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+	 */
+	@Override
+	public void sessionCreated(HttpSessionEvent arg0) {
+		System.out.println("Sesión creada " + arg0.getSession().getAttribute("visitas"));
+	}
+
+	/**
+	 * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+	 */
+	@Override
+	public void sessionDestroyed(HttpSessionEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+}
