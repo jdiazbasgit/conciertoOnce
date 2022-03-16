0c6924d2977c3bce5086b3aec2e742cf3ea35aa0
Mar Almagro <maralmagromoreno@gmail.com>
2022-03-16 19:27:09 +0100

    spring.xml added

--- src/main/java/ejemplomaven/ConciertoSolista.java
+++ src/main/java/ejemplomaven/ConciertoSolista.java
@@ -1,4 +1,4 @@
-package ejemploMaven;
+package ejemplomaven;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,14 +8,15 @@ import concierto.musicos.Solista;
 
 public class ConciertoSolista {
 
+	private static final String SPRING_CONF_XML_FILE="spring.xml";
 	public static void main(String[] args) {
-		
-		ApplicationContext habitacion= new ClassPathXmlApplicationContext("spring.xml");
-		Solista solista= (Solista) habitacion.getBean("solista");
+	
+		//try (ConfigurableApplicationContext habitacion=new ClassPathXmlApplicationContext(SPRING_CONF_XML_FILE)){
 		try {
+			ApplicationContext habitacion=new ClassPathXmlApplicationContext(SPRING_CONF_XML_FILE);
+			Solista solista= habitacion.getBean(Solista.class);		
 			solista.tocar();
 		} catch (SinSonidoException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
