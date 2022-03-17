0da3640a434e876da791ddc6b5419aa9ab25935b
JuanVega <juanvegarosa2@gmail.com>
2022-03-17 12:16:45 +0100

    Solución de errores y problemas menores.

--- src/concierto/teatros/ConciertoSolista.java
+++ src/concierto/teatros/ConciertoSolista.java
@@ -1,4 +1,5 @@
 package concierto.teatros;
+
 import concierto.beans.InstrumentoBean;
 import concierto.excepciones.SinSonidoException;
 import concierto.instrumentos.Instrumento;
@@ -8,33 +9,28 @@ public class ConciertoSolista {
 
 	public static void main(String[] args) {
 
-	
-		
-		
-		
-		/*InstrumentoInterface instrumentoJavier = new InstrumentoJavier("sonido de instrumento Javier");
-		MusicoInterface musicoJavier = new MusicoJavier(instrumentoJavier);
-		musicoJavier.tocar();*/
-		
+		/*
+		 * InstrumentoInterface instrumentoJavier = new
+		 * InstrumentoJavier("sonido de instrumento Javier"); MusicoInterface
+		 * musicoJavier = new MusicoJavier(instrumentoJavier); musicoJavier.tocar();
+		 */
+
 		Solista solista = new Solista(new Instrumento(new InstrumentoBean("nada")));
-		Solista solista1 = new Solista(new Instrumento(new InstrumentoBean("blam blam")));
-		Solista solista2 = new Solista(new Instrumento(new InstrumentoBean("blum blum")));
-		Solista solista3 = new Solista(new Instrumento(new InstrumentoBean("fuaa")));
-		Solista solista4 = new Solista(new Instrumento(new InstrumentoBean("plim")));
-		Solista solista5 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
-		Solista solista6 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
-		Solista solista7 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
-		Solista solista8 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
-		Solista solista9 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
-		
+//		Solista solista1 = new Solista(new Instrumento(new InstrumentoBean("blam blam")));
+//		Solista solista2 = new Solista(new Instrumento(new InstrumentoBean("blum blum")));
+//		Solista solista3 = new Solista(new Instrumento(new InstrumentoBean("fuaa")));
+//		Solista solista4 = new Solista(new Instrumento(new InstrumentoBean("plim")));
+//		Solista solista5 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
+//		Solista solista6 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
+//		Solista solista7 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
+//		Solista solista8 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
+//		Solista solista9 = new Solista(new Instrumento(new InstrumentoBean("sonido")));
+
 		try {
 			solista.tocar();
 		} catch (SinSonidoException e) {
 			System.out.println("Señores el instrumento se ha roto y se ha acabado el concieto");
 		}
-		
-		
-		
 
 	}
 
