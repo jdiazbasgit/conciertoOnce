ce59f56004833c11b379fa87aea74c4ac301b592
Your Name <you@example.com>
2022-03-18 11:49:32 +0100

    examen java

---
+++ src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Operaciones operaciones = new Operaciones(20, 20);
+		System.out.println("El resultado es: " + operaciones.suma());
+	}
+
+}
