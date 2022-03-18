f35adfde7d9e6d36deda43dee745600cd9d4b7a2
JuanVega <juanvegarosa2@gmail.com>
2022-03-18 11:50:29 +0100

    examen de JuanVega finalizado

---
+++ src/examenJuan/Ejecución.java
@@ -0,0 +1,11 @@
+package examenJuan;
+
+public class Ejecución {
+
+	public static void main(String[] args) {
+		Operaciones operaciones= new Operaciones(5, 4);
+		System.out.println(operaciones.suma());
+
+	}
+
+}
---
+++ src/examenJuan/Operaciones.java
@@ -0,0 +1,34 @@
+package examenJuan;
+
+public class Operaciones {
+private int numero1;
+private int numero2;
+
+public Operaciones(int numero1, int numero2) {
+	this.numero1= numero1;
+	this.numero2= numero2;
+	
+}
+	public int suma() {
+		int resultado= getNumero1()+getNumero2();
+		return resultado;
+	}
+	
+	public int getNumero1() {
+		return numero1;
+	}
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
+	}
+	public int getNumero2() {
+		return numero2;
+	}
+	public void setNumero2(int numero2) {
+		this.numero2 = numero2;
+	}
+	
+	
+	
+	
+	
+}
