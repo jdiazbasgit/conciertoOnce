bdaaee29637d568404fbedc194ce8390b1851513
Your Name <you@example.com>
2022-03-18 11:51:41 +0100

    examen java

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		Operaciones operaciones = new Operaciones(20, 20);
-		System.out.println("El resultado es: " + operaciones.suma());
+		System.out.println("El resultado de la suma es: " + operaciones.suma());
 	}
 
 }
