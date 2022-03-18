0e170e176555f85280d6b700920604ecb230f020
unknown <you@example.com>
2022-03-18 12:57:49 +0100

    examen

---
+++ src/examen/Operaciones.java
@@ -0,0 +1,6 @@
+package examen;
+
+public class Operaciones {
+
+}
+No me acuerdo de los comandos del código de memoria, tengo que consultar la documentación. Si hubiera repasado en casa, me acordaría mejor supongo.
