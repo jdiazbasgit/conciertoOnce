dbf5aa2d1a6653fd2b8ebca25741c53f6a92c2a6
Your Name <you@example.com>
2022-03-18 12:18:37 +0100

    Ejercicio optimizado

--- src/EjecucionExamen.java
+++ src/EjecucionExamen.java
@@ -3,9 +3,9 @@ public class EjecucionExamen {
 
 	public static void main(String[] args) {
 		
-	Operaciones operaciones = new Operaciones(20,10);
+	Operaciones operaciones = new Operaciones(20,20);
 	
-	operaciones.suma(operaciones.getNuemero1(),operaciones.getNumero2());
+	operaciones.suma();
 
 	}
 
--- src/Operaciones.java
+++ src/Operaciones.java
@@ -4,24 +4,24 @@ public class Operaciones {
 	private int numero1;
 	private int numero2;
 	
-	public Operaciones(int nuemero1, int numero2) {
+	public Operaciones(int numero1, int numero2) {
 		super();
-		this.numero1 = nuemero1;
+		this.numero1 = numero1;
 		this.numero2 = numero2;
 	}
 	
-	public void suma(int numero1,int numero2) {
+	public void suma() {
 		int resultado=0;
-		resultado=numero1+numero2;
-		System.out.println("La suma de: " + getNuemero1() + " + " + getNumero2() + " = " + resultado);
+		resultado=getNumero1()+getNumero2();
+		System.out.println("La suma de: " + getNumero1() + " + " + getNumero2() + " = " + resultado);
 	}
 
-	public int getNuemero1() {
+	public int getNumero1() {
 		return numero1;
 	}
 
-	public void setNuemero1(int nuemero1) {
-		this.numero1 = nuemero1;
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
 	}
 
 	public int getNumero2() {
@@ -31,5 +31,7 @@ public class Operaciones {
 	public void setNumero2(int numero2) {
 		this.numero2 = numero2;
 	}
+
+	
 
 }
