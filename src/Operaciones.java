0c876eea4ae0362415485296c63a77a62dbc5625
Your Name <you@example.com>
2022-03-18 12:23:08 +0100

    examen

---
+++ src/Operaciones.java
@@ -0,0 +1,26 @@
+
+public class Operaciones {
+	
+	private int numero1=2;
+	private int numero2=3;
+	
+public Operaciones(int numero1, int numero2) {
+		super();
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	
+
+
+	}
+
+
+public void suma() {
+	
+}
+
+
+
+	
+}
+
+
---
+++ src/Suma.java
@@ -0,0 +1,16 @@
+
+public class Suma {
+ private int numero1;
+ private int numero2;
+ 
+ 
+ 
+ @SuppressWarnings("unused")
+public static void main(String[] args) {
+
+  Operaciones operaciones=new Operaciones(2,3);
+
+System.out.println(2+3);
+
+}
+}
