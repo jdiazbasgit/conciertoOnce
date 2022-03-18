62fb5e281772050261d96fd3708336e635b3fdab
Your Name <you@example.com>
2022-03-18 12:01:52 +0100

    examen sorpresa

---
+++ src/examen/Operaciones.java
@@ -0,0 +1,19 @@
+package examen;
+
+public class Operaciones {
+	
+	private int numero1;
+	private int numero2;
+	
+	public Operaciones(int numero1, int numero2) {
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	}
+	
+	public int suma() {
+		
+		return numero1 + numero2;
+	}
+	
+
+}
---
+++ src/main/VerResultadoFinal.java
@@ -0,0 +1,12 @@
+package main;
+import examen.Operaciones;
+
+public class VerResultadoFinal {
+
+	public static void main(String[] args) {
+
+		Operaciones operacion = new Operaciones(8, 9);
+		System.out.println(operacion.suma());
+	}
+
+}
