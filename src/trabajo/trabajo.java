67540da4dc0986985a07f133425eec78b2f30f80
JACINTO SANCHEZ MACIAS <jacinto_sanchez@hotmail.com>
2022-03-18 11:54:31 +0100

    examen

---
+++ src/trabajo/trabajo.java
@@ -0,0 +1,14 @@
+package trabajo;
+
+import examen.Operaciones;
+
+public class trabajo {
+
+	public static void main(String[] args) {
+		
+		Operaciones suma = new Operaciones(8, 8);
+		System.out.println(suma.sumaNumeros(8, 8));
+
+	}
+
+}
