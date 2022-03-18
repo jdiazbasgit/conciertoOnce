4ecb03ee0c4843a75cf9429a52e32a56551b78db
Your Name <you@example.com>
2022-03-18 12:44:26 +0100

    examen no terminado

--- src/operaciones/EjecutarOperacion.java
+++ src/operaciones/EjecutarOperacion.java
@@ -1,25 +1,39 @@
 package operaciones;
 
-
 public class EjecutarOperacion
 {
-	public static void main(String[] args)
-    {
-    	
-    	 
-    	 int n1;
-    	 int n2;
-		 int numero1 = (n1);
-    	 int numero2 = (n2);
-    	 
-    	 int resultado = numero1 + numero2;
-    	 
-    	 System.out.println("El resultado es el siguiente");
-    	 
-    	 
-    }
-}
+
+	private int numero1;
+	private int numero2;
+
+	public EjecutarOperacion(int numero1, int numero2) {
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	}
 
 	
 
+	public int suma(){
+		int resultado = getNumero1()+getNumero2();
+		 return resultado;
+	 }
+	public int getNumero1() {
+		return numero1;
+	}
 
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
+	}
+
+	public int getNumero2() {
+		return numero2;
+	}
+
+	public void setNumero2(int numero2) {
+		this.numero2 = numero2;
+	}
+
+}
+
+}
+}
