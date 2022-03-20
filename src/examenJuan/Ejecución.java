ed1601507278fd24308483d1f23b9ca05fb5d7ab
JuanVega <juanv@LegionJuan>
2022-03-20 11:32:26 +0100

    Revisión del ejercicio y práctica.

--- src/examenJuan/Ejecución.java
+++ src/examenJuan/Ejecución.java
@@ -3,9 +3,18 @@ package examenJuan;
 public class Ejecución {
 
 	public static void main(String[] args) {
-		Operaciones operaciones= new Operaciones(5, 40);
-		System.out.println(operaciones.suma());
+		System.out.println(new Operaciones(5, 2).suma());
+		System.out.println(new Operaciones().resta(6, 2));
+		System.out.println(new Operaciones(99, 3).division());
+		System.out.println(new Operaciones(3, 5).multi());
+
+		Operaciones operaciones = new Operaciones(5, 40);
+		System.out.println("El resultado de sumar " + operaciones.getNumero1() + " y " + operaciones.getNumero2()
+				+ " es igual a " + operaciones.suma());
+
+		Operaciones operaciones2 = new Operaciones(12, 3);
+		System.out.println("El resultado de restar " + operaciones2.getNumero1() + " y " + operaciones2.getNumero2()
+				+ " es igual a " + operaciones2.otraResta());
 
 	}
-
 }
--- src/examenJuan/Operaciones.java
+++ src/examenJuan/Operaciones.java
@@ -1,33 +1,51 @@
 package examenJuan;
 
 public class Operaciones {
-private int numero1;
-private int numero2;
+	private int numero1;
+	private int numero2;
 
-public Operaciones(int numero1, int numero2) {
-	this.numero1= numero1;
-	this.numero2= numero2;
-	
-}
 	public int suma() {
-		return getNumero1()+getNumero2();
+		return getNumero1() + getNumero2();
 	}
-	
+
+	public int resta(int resta1, int resta2) {
+		return resta1 - resta2;
+	}
+
+	public int otraResta() {
+		return getNumero1() - getNumero2();
+	}
+
+	public int multi() {
+		return getNumero1() * getNumero2();
+	}
+
+	public double division() {
+		return getNumero1() / getNumero2();
+	}
+
+	public Operaciones() {
+	}
+
+	public Operaciones(int numero1, int numero2) {
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	}
+
 	public int getNumero1() {
 		return numero1;
 	}
+
 	public void setNumero1(int numero1) {
 		this.numero1 = numero1;
 	}
+
 	public int getNumero2() {
 		return numero2;
 	}
+
 	public void setNumero2(int numero2) {
 		this.numero2 = numero2;
 	}
-	
-	
-	
-	
-	
+
 }
