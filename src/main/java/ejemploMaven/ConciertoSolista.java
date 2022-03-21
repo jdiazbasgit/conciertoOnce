0dc8e4b4bbbb0b7fb0c8b9e29ec1e24784a29673
Your Name <you@example.com>
2022-03-21 11:25:23 +0100

    cambios en la clase concierto solista

--- src/main/java/ejemploMaven/ConciertoSolista.java
+++ src/main/java/ejemploMaven/ConciertoSolista.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import concierto.excepciones.SinSonidoException;
+import concierto.musicos.MusicoInterface;
 import concierto.musicos.Solista;
 
 public class ConciertoSolista {
@@ -11,7 +12,7 @@ public class ConciertoSolista {
 	public static void main(String[] args) {
 		
 		ApplicationContext habitacion= new ClassPathXmlApplicationContext("spring.xml");
-		Solista solista= (Solista) habitacion.getBean("solista");
+		MusicoInterface solista= (MusicoInterface) habitacion.getBean("solista");
 		try {
 			solista.tocar();
 		} catch (SinSonidoException e) {
