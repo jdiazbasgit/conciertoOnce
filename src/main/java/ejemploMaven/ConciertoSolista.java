9bd121edf76e421585bd1eaf06ac5a081c0d6a2f
JuanVega <juanvegarosa2@gmail.com>
2022-03-21 13:35:39 +0100

    0iuy

--- src/main/java/ejemploMaven/ConciertoSolista.java
+++ src/main/java/ejemploMaven/ConciertoSolista.java
@@ -4,7 +4,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import concierto.excepciones.SinSonidoException;
+import concierto.musicos.HombreOrquesta;
 import concierto.musicos.MusicoInterface;
+import concierto.musicos.Solista;
 
 public class ConciertoSolista {
 
@@ -12,11 +14,15 @@ public class ConciertoSolista {
 		
 		ApplicationContext habitacion= new ClassPathXmlApplicationContext("springAnotaciones.xml");
 		MusicoInterface solista= (MusicoInterface) habitacion.getBean("solista");
-		MusicoInterface hombreOrquesta= (MusicoInterface)habitacion.getBean("hombreOrquesta");
+		//Solista solista1= (Solista) habitacion.getBean("solista1");
+		MusicoInterface hombreOrquesta= (MusicoInterface) habitacion.getBean("hombreOrquesta");
 		try {
 			solista.tocar();
+			
+			
 			hombreOrquesta.tocar();
 		} catch (SinSonidoException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
