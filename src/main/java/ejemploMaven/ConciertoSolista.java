b57a15999359e2ee59665bb181da9e3301910086
JuanVega <juanvegarosa2@gmail.com>
2022-03-21 11:28:10 +0100

    dfghj

--- src/main/java/ejemploMaven/ConciertoSolista.java
+++ src/main/java/ejemploMaven/ConciertoSolista.java
@@ -4,19 +4,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import concierto.excepciones.SinSonidoException;
+import concierto.musicos.MusicoInterface;
 import concierto.musicos.Solista;
 
 public class ConciertoSolista {
 
-	private static ApplicationContext habitacion;
-
 	public static void main(String[] args) {
-
-		habitacion = new ClassPathXmlApplicationContext("spring.xml");
-		Solista solista = (Solista) habitacion.getBean("solista");
+		
+		ApplicationContext habitacion= new ClassPathXmlApplicationContext("spring.xml");
+		MusicoInterface solista= (MusicoInterface) habitacion.getBean("solista");
 		try {
 			solista.tocar();
 		} catch (SinSonidoException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
