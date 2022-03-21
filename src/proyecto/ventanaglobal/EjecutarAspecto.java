b8f3670ad49834fc026934f036a80703118014e8
juanc <juanc@DESKTOP-PEVOBNK.home>
2022-03-22 00:32:52 +0100

    m

--- src/proyecto/ventanaglobal/EjecutarAspecto.java
+++ src/proyecto/ventanaglobal/EjecutarAspecto.java
@@ -1,27 +1,9 @@
-package proyecto.ventanaglobal;
-
-import java.sql.SQLException;
-import java.util.List;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import empresa.beans.DatosPersonales;
-import empresa.daos.DatosPersonalsDaoInterface;
 
 public class EjecutarAspecto {
 
 	public static void main(String[] args) {
-		ApplicationContext habitacion= new ClassPathXmlApplicationContext("spring.xml");
-		DatosPersonalsDaoInterface dao= (DatosPersonalsDaoInterface	) habitacion.getBean("datosPersonalesDao");
-		
-		try {
-			List<DatosPersonales> datosPersonales= dao.dameDatosPersonales();
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
+
 	}
 
 }
