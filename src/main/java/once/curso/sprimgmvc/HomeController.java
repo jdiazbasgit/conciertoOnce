ea472c61b2c75289c4da254ea73b3e428b06bd7e
Your Name <you@example.com>
2022-03-22 12:57:04 +0100

    cambio en el controller, añadido metodo para que devuelva string a traves de un enlace

--- src/main/java/once/curso/sprimgmvc/HomeController.java
+++ src/main/java/once/curso/sprimgmvc/HomeController.java
@@ -4,8 +4,12 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,24 +20,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
 		return "home";
 	}
+
+	@RequestMapping("algo")
+	public String algo(HttpServletRequest request, Model model) {
+		String texto = request.getParameter("texto");
+
+		model.addAttribute("algo", "has escrito " + texto.toUpperCase() + "y tiene " + texto.length() + " letras");
+		return "algo";
+	}
+
 	
 }
