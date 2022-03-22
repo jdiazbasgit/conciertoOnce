4bc0c528db2cd886323ccfc8cb922363464fe1db
Your Name <you@example.com>
2022-03-22 14:24:17 +0100

    cambios, variables pasadas por url

--- src/main/java/once/curso/sprimgmvc/HomeController.java
+++ src/main/java/once/curso/sprimgmvc/HomeController.java
@@ -4,16 +4,14 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -40,11 +38,17 @@ public class HomeController {
 		return "home";
 	}
 
-	@RequestMapping("algo")
-	public String algo(HttpServletRequest request, Model model) {
-		String texto = request.getParameter("texto");
+	@RequestMapping(value="algo",method = RequestMethod.GET)
+	public String algo( Model model,@RequestParam String texto,@RequestParam String texto2) {
+		
+		model.addAttribute("algo", "has escrito " + texto2.toUpperCase() + "y tiene " + texto.length() + " letras");
+		return "algo";
+	}
+	@RequestMapping("algo/{texto}/{texto2}")
+	public String algo1( Model model,@PathVariable String texto,@PathVariable String texto2) {
+		
 
-		model.addAttribute("algo", "has escrito " + texto.toUpperCase() + "y tiene " + texto.length() + " letras");
+		model.addAttribute("algo", "tu nombre y apellido es "+ texto+ " " + texto2);
 		return "algo";
 	}
 
