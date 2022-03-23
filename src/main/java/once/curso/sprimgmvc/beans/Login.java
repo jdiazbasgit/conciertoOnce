5c10647871715646536a4235b80e21880d0c7df7
Antonio Peinado Sevilla <a.peinado.sevilla@gmail.com>
2022-03-23 11:11:33 +0100

    formu

---
+++ src/main/java/once/curso/sprimgmvc/beans/Login.java
@@ -0,0 +1,26 @@
+package once.curso.sprimgmvc.beans;
+
+
+	
+	public class Login {
+		
+		
+		private String clave;
+		private String usuario;
+		
+		
+		public String getUsuario() {
+			return usuario;
+		}
+		public void setUsuario(String usuario) {
+			this.usuario = usuario;
+		}
+		public String getClave() {
+			return clave;
+		}
+		public void setClave(String clave) {
+			this.clave = clave;
+		}
+		
+	}
+	
