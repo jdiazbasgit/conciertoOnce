e406c87b1bddadd00cfcc89d15a9c1b60dc43ea4
Carlos <vara.carlos.superior@gmail.com>
2022-03-23 11:42:35 +0100

    OK y KO con Beans de Logeo

--- src/main/java/once/curso/sprimgmvc/HomeController.java
+++ src/main/java/once/curso/sprimgmvc/HomeController.java
@@ -10,74 +10,93 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import once.curso.sprimgmvc.beans.Login;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	
+	/*
+	 * @RequestMapping(value = "/", method = RequestMethod.GET) public ModelAndView
+	 * home(ModelAndView modelAndView) { modelAndView.setViewName("home"); Login
+	 * login= new Login(); login.setUsuario("federico");
+	 * modelAndView.addObject("login", login); return modelAndView; }
+	 */
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		//logger.error("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public ModelAndView home(ModelAndView modelAndView) {
+		modelAndView.setViewName("home");
+		Login login= new Login();
+		//login.setUsuario("federico");
+		modelAndView.addObject("login", login);
+		return modelAndView;
 	}
-	
-	
-	
-	//@RequestMapping("algo")
+
 	@RequestMapping(value = "algo", method = RequestMethod.GET)
-	public String algo(Model model,@RequestParam("texto") String texto,@RequestParam String texto2) {
-		
-		/*
-		 * String texto = request.getParameter("texto"); String texto1 =
-		 * request.getParameter("texto1"); String texto2 =
-		 * request.getParameter("texto2");
-		 * model.addAttribute("algo","has escrito "+texto1.toUpperCase()+" y "+texto2.
-		 * toUpperCase()+ " tienes "+(texto1.length()+texto2.length())+" letras");
-		 */
-		model.addAttribute("algo","has escrito "+texto.toUpperCase()+" y "+texto2.toUpperCase()+ " tienes "+(texto.length()+texto2.length())+" letras");
-		
+	public String algo(Model model, @RequestParam String texto, @RequestParam String texto2) {
+
+		model.addAttribute("algo", "has escrito " + texto2.toUpperCase() + " y tiene " + texto.length() + " letras");
 
 		return "algo";
+
 	}
-	
-	
-	
+
 	@RequestMapping("algo/{texto}/{texto2}")
-	public String algo1(Model model,@PathVariable("texto") String texto,@PathVariable String texto2) {
-		
-		/*
-		 * String texto = request.getParameter("texto"); String texto1 =
-		 * request.getParameter("texto1"); String texto2 =
-		 * request.getParameter("texto2");
-		 * model.addAttribute("algo","has escrito "+texto1.toUpperCase()+" y "+texto2.
-		 * toUpperCase()+ " tienes "+(texto1.length()+texto2.length())+" letras");
-		 */
-		model.addAttribute("algo","has escrito "+texto.toUpperCase()+" y "+texto2.toUpperCase()+ " tienes "+(texto.length()+texto2.length())+" letras");
-		
+	public String algo(Model model, @PathVariable("texto") String nombre, @PathVariable String texto2,
+			@RequestParam String primerNombre) {
+
+		model.addAttribute("variable",
+				"tu nombre y apellido es " + nombre + " " + texto2 + " y tu primer nombre es " + primerNombre);
 
 		return "algo";
+
+	}
+
+	
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public ModelAndView login(ModelAndView modelAndView, @RequestParam String usuario, @RequestParam String clave) {
+
+		modelAndView.setViewName("ko");
+		if (usuario.equals("pepe") && clave.equals("1234"))
+			modelAndView.setViewName("ok");
+		return modelAndView;
 	}
 	
 	
-	
+	@RequestMapping(value = "loginConObjeto", method = RequestMethod.POST)
+	public ModelAndView loginConObjeto(ModelAndView modelAndView, Login login) {
+		modelAndView.setViewName("ko");
+		if (login.getUsuario().equals("pepe") && login.getClave().equals("1234"))
+			modelAndView.setViewName("ok");
+		return modelAndView;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
