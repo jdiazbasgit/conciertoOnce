e20729746b56a9a1698907d449cdeb974434f0bc
Antonio Peinado Sevilla <a.peinado.sevilla@gmail.com>
2022-03-23 09:28:24 +0100

    terminado

--- src/main/java/once/curso/sprimgmvc/HomeController.java
+++ src/main/java/once/curso/sprimgmvc/HomeController.java
@@ -10,8 +10,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -24,9 +27,10 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/pepe", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.error("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -38,15 +42,28 @@ public class HomeController {
 		return "home";
 	}
 	
-	@RequestMapping("algo")
-	public String algo(Model model,HttpServletRequest request) {
+	@RequestMapping(value="algo",method = RequestMethod.GET)
+	public String algo(Model model, @RequestParam String texto,@RequestParam String texto2) {
 		
-		String texto=request.getParameter("texto");
-		model.addAttribute("algo","has escrito "+texto.toUpperCase()+ " y tiene "+texto.length()+" letras");
+		
+		model.addAttribute("algo",
+				"has escrito "+texto2.toUpperCase()+ " y tiene "+texto.length()+" letras");
 		
 		return "algo";
 		
 	}
 	
 	
+	@RequestMapping("algo/{texto}/{texto2}")
+	public String algo1(Model model, @PathVariable("texto") String nombre,@PathVariable String texto2) {
+		
+		
+		model.addAttribute("algo",
+				"tu nombre y apellido es "+nombre+" "+texto2);
+		
+		return "algo";
+		
+	}
+	
 }
+
--- src/main/webapp/WEB-INF/views/algo.jsp
+++ src/main/webapp/WEB-INF/views/algo.jsp
@@ -4,9 +4,10 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>texto</title>
 </head>
 <body>
+<h1>Hello world! </h1>
 <h1>${algo} </h1>
 </body>
 </html>
