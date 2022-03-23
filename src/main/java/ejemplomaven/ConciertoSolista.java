e4d93f862fb5b9c4811c9cf2cf0eca0d1ba4eabe
Mar <maralmagromoreno@gmail.com>
2022-03-23 14:20:52 +0100

    modified springAnotaciones.xml

--- src/main/java/ejemplomaven/ConciertoSolista.java
+++ src/main/java/ejemplomaven/ConciertoSolista.java
@@ -1,22 +1,26 @@
 package ejemplomaven;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import concierto.configuracion.Configuracion;
 import concierto.excepciones.SinSonidoException;
 import concierto.musicos.MusicoInterface;
 
 public class ConciertoSolista {
 
 	public static void main(String[] args) {
-		
-		ApplicationContext habitacion= new ClassPathXmlApplicationContext("springAnotaciones.xml");
-		MusicoInterface solista= (MusicoInterface) habitacion.getBean("solista");
+
+		// ApplicationContext habitacion= new
+		// ClassPathXmlApplicationContext("springAnotaciones.xml");
+		ApplicationContext habitacion = new AnnotationConfigApplicationContext(Configuracion.class);
+		MusicoInterface solista = (MusicoInterface) habitacion.getBean("solista");
 		MusicoInterface hombreOrquesta = (MusicoInterface) habitacion.getBean("hombreOrquesta");
 		try {
 			solista.tocar();
 			hombreOrquesta.tocar();
 		} catch (SinSonidoException e) {
-			e.printStackTrace();
+			// e.printStackTrace();
 		}
 	}
 
