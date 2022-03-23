8c78a6bd5709547bbc08f9606bca76b781fc6329
Antonio Peinado Sevilla <a.peinado.sevilla@gmail.com>
2022-03-23 11:15:59 +0100

    librerias

--- src/main/java/once/curso/sprimgmvc/beans/Login.java
+++ src/main/java/once/curso/sprimgmvc/beans/Login.java
@@ -1,26 +1,11 @@
 package once.curso.sprimgmvc.beans;
 
+import lombok.Data;
 
-	
-	public class Login {
-		
-		
-		private String clave;
-		private String usuario;
-		
-		
-		public String getUsuario() {
-			return usuario;
-		}
-		public void setUsuario(String usuario) {
-			this.usuario = usuario;
-		}
-		public String getClave() {
-			return clave;
-		}
-		public void setClave(String clave) {
-			this.clave = clave;
-		}
-		
-	}
-	
+@Data
+public class Login {
+
+	private String clave;
+	private String usuario;
+
+}
