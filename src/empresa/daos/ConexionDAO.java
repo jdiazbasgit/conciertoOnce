7217145d5b9d7409409e038a6781b143144f4939
JuanVega <juanv@LegionJuan>
2022-03-30 00:58:48 +0200

    Creación de rama desde bd/profe

--- src/empresa/daos/ConexionDAO.java
+++ src/empresa/daos/ConexionDAO.java
@@ -10,9 +10,9 @@ import com.mysql.cj.jdbc.Driver;
 public class ConexionDAO {
 	private static final String NOMBRE_BD="curso";
 	private static final String PROTOCOLO="jdbc:mysql:/";
-	private static final String DOMINIO="/192.168.10.49:3306/";
+	private static final String DOMINIO="/localhost:3306/";
 	private static final String URL = PROTOCOLO+DOMINIO+NOMBRE_BD;
-	private static final String USUARIO = "curso";
+	private static final String USUARIO = "root";
 	private static final String PASSWORD = "Cursocurso1;";
 	
 
