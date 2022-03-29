9570592b5d3d45c00825ba16edce70cb83c3a594
JuanVega <juanvegarosa2@gmail.com>
2022-03-29 09:51:14 +0200

    Proyecto finalizado+

--- src/main/java/once/curso/practicaspringmvc/config/MvcConfiguration.java
+++ src/main/java/once/curso/practicaspringmvc/config/MvcConfiguration.java
@@ -3,17 +3,11 @@ package once.curso.practicaspringmvc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import empresa.daos.ConexionDAO;
-import empresa.daos.DatosPersonalesDao;
 import empresa.daos.EmpresaDAO;
 import empresa.daos.HijoDAO;
-import empresa.daos.TipoCarnetDao;
 import empresa.daos.VehiculoDao;
 
 @Configuration
@@ -38,11 +32,6 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 	public VehiculoDao getVehiculo() {
 		return new VehiculoDao();
 	}
-
-	@Bean
-	public TipoCarnetDao getTipoCarnetDao() {
-		return new TipoCarnetDao();
-	}
 	
 	@Bean
 	public HijoDAO gethiHijoDAO() {
@@ -54,14 +43,9 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 		return new EmpresaDAO();	
 		}
 	
-	@Bean
-	public DatosPersonalesDao getdaDatosPersonalesDao() {
-		return new DatosPersonalesDao();
-	}
-	
-	
-	@Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}
+//	
+//	@Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//	}
 
 }
--- src/main/java/once/curso/practicaspringmvc/controller/HomeRestController.java
+++ src/main/java/once/curso/practicaspringmvc/controller/HomeRestController.java
@@ -7,15 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import empresa.beans.DatosPersonales;
 import empresa.beans.Empresa;
 import empresa.beans.Hijo;
-import empresa.beans.TipoCarnet;
 import empresa.beans.Vehiculo;
-import empresa.daos.DatosPersonalesDao;
 import empresa.daos.EmpresaDAO;
 import empresa.daos.HijoDAO;
-import empresa.daos.TipoCarnetDao;
 import empresa.daos.VehiculoDao;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,40 +23,28 @@ public class HomeRestController {
 
 	@Autowired
 	private VehiculoDao vehiculoDao;
-	@Autowired
-	private TipoCarnetDao tipoCarnetDao;
+
 	@Autowired
 	private HijoDAO hijoDAO;
 	@Autowired
 	private EmpresaDAO empresaDAO;
-	@Autowired
-	private DatosPersonalesDao datosPersonalesDao;
-	
 
 	
 	@GetMapping("/vehiculos")
 	public List<Vehiculo> getVehiculosDAO() throws SQLException {
 		return vehiculoDao.dameVehiculos();
 	}
-	
-	@GetMapping("/carnets")
-	public List<TipoCarnet> getTiposCarnetDAO() throws SQLException {
-		return tipoCarnetDao.dameTipoCarnet();
-		
-	}
+
 	@GetMapping("/hijos")
 	public List<Hijo> getHijosDAO() throws SQLException {
 		return hijoDAO.dameListaHijos();
-		
+
 	}
+
 	@GetMapping("/empresa")
 	public List<Empresa> getEmpresasDAO() throws SQLException {
 		return empresaDAO.dameEmpresas();
-		
-	}
-	@GetMapping("/datosPersonales")
-	public List<DatosPersonales> getDatosPersonalesDAO() throws SQLException {
-		return datosPersonalesDao.dameDatosPersonales();
+
 	}
 
 }
